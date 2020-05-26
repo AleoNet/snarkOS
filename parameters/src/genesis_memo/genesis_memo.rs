0e0286e28a0973f3d43f977e5876d6bd1a1b06ba
raychu86 <raychu86@gmail.com>
2020-05-26 10:42:43 -0700

    Update genesis memo size

--- parameters/src/genesis_memo/genesis_memo.rs
+++ parameters/src/genesis_memo/genesis_memo.rs
@@ -4,7 +4,7 @@ pub struct GenesisMemo;
 
 impl Parameter for GenesisMemo {
     const CHECKSUM: &'static str = "";
-    const SIZE: u64 = 64;
+    const SIZE: u64 = 32;
 
     fn load_bytes() -> Vec<u8> {
         let buffer = include_bytes!("memo.genesis");
