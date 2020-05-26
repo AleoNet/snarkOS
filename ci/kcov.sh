4539f2919a3c343add8accbf0f387f8088facacd
Daniil Ryzhkov <daniil.re@gmail.com>
2020-05-26 23:55:19 +0300

    Attempt to run kcov directly on executor

--- ci/kcov.sh
+++ ci/kcov.sh
@@ -1,6 +1,8 @@
-cd /opt/kcov-source/ && ls && for file in target/debug/*-*[^\.d];
-  do
-    echo $file;
-    mkdir -p "target/cov/$(basename $file)";
-    kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file";
-  done
+wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz &&
+tar xzf master.tar.gz && cd kcov-master &&
+mkdir build && cd build && cmake .. && make && sudo make install &&
+cd ../.. && sudo rm -rf kcov-master && sudo rm -rf master.tar.gz &&
+sudo rm -rf target/debug/base_dpc* && sudo rm -rf target/debug/consensus_dpc* &&
+for file in target/debug/*-*[^\.d]; do mkdir -p "target/cov/$(basename $file)"; /usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; done &&
+bash <(curl -s https://codecov.io/bash) &&
+echo "Uploaded code coverage"
