8cff9ed2dd735eca5ef75988b631c85e7c54073c
Daniil Ryzhkov <daniil.re@gmail.com>
2020-05-27 01:02:03 +0300

    Trying docker approach again

--- ci/kcov.sh
+++ ci/kcov.sh
@@ -1,8 +1,6 @@
-wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz &&
-tar xzf master.tar.gz && cd kcov-master &&
-mkdir build && cd build && cmake .. && make && sudo make install &&
-cd ../.. && sudo rm -rf kcov-master && sudo rm -rf master.tar.gz &&
-sudo rm -rf target/debug/base_dpc* && sudo rm -rf target/debug/consensus_dpc* &&
-for file in target/debug/*-*[^\.d]; do mkdir -p "target/cov/$(basename $file)"; /usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; done &&
-bash <(curl -s https://codecov.io/bash) &&
-echo "Uploaded code coverage"
+cd /opt/kcov-source &&
+rm -rf target/debug/base_dpc* && rm -rf target/debug/consensus_dpc* &&
+for file in target/debug/*-*[^\.d];
+  do mkdir -p "target/cov/$(basename $file)";
+    /usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file";
+  done
