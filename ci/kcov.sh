c97154e19e825d35e62f9d050d70a99bbb980ba2
Daniil Ryzhkov <daniil.re@gmail.com>
2020-05-26 20:26:29 +0300

    Attempt to split circle ci into separate step for coverage information analysis

--- ci/kcov.sh
+++ ci/kcov.sh
@@ -1,5 +1,6 @@
-cd /source && ls && for file in target/debug/*-*[^\.d];
+cd /opt/kcov-source/ && ls && for file in target/debug/*-*[^\.d];
   do
+    echo $file;
     mkdir -p "target/cov/$(basename $file)";
     kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file";
   done
