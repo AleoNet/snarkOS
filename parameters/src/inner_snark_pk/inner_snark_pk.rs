1e61d76a5384b56b06c87c064356d7f9257b2ac7
raychu86 <raychu86@gmail.com>
2020-05-26 18:23:10 -0700

    Fix parameter trait attribute ordering

--- parameters/src/inner_snark_pk/inner_snark_pk.rs
+++ parameters/src/inner_snark_pk/inner_snark_pk.rs
@@ -3,8 +3,8 @@ use snarkos_models::parameters::Parameter;
 pub struct InnerSNARKPKParameters;
 
 impl Parameter for InnerSNARKPKParameters {
-    const SIZE: u64 = 0;
     const CHECKSUM: &'static str = "";
+    const SIZE: u64 = 0;
 
     fn load_bytes() -> Vec<u8> {
         let buffer = include_bytes!("./inner_snark_pk.params");
--- parameters/src/outer_snark_pk/outer_snark_pk.rs
+++ parameters/src/outer_snark_pk/outer_snark_pk.rs
@@ -3,8 +3,8 @@ use snarkos_models::parameters::Parameter;
 pub struct OuterSNARKPKParameters;
 
 impl Parameter for OuterSNARKPKParameters {
-    const SIZE: u64 = 0;
     const CHECKSUM: &'static str = "";
+    const SIZE: u64 = 0;
 
     fn load_bytes() -> Vec<u8> {
         let buffer = include_bytes!("./outer_snark_pk.params");
