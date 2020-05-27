8dbd8cdc180577c0be51deceee2227d7dccbd2f2
raychu86 <raychu86@gmail.com>
2020-05-27 15:25:01 -0700

    Fix parameter trait attribute ordering

--- models/src/parameters/parameters.rs
+++ models/src/parameters/parameters.rs
@@ -1,8 +1,8 @@
 use snarkos_errors::parameters::ParametersError;
 
 pub trait Parameters {
-    const SIZE: u64;
     const CHECKSUM: &'static str;
+    const SIZE: u64;
 
     fn load_bytes() -> Result<Vec<u8>, ParametersError>;
 }
