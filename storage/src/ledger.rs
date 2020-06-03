532d69507d6c11c47c7371634dde10984675b2ec
raychu86 <raychu86@gmail.com>
2020-06-02 21:32:36 -0700

    Update ledger.rs to ledger_scheme.rs

--- storage/src/ledger.rs
+++ storage/src/ledger.rs
@@ -183,10 +183,6 @@ impl<T: Transaction, P: MerkleParameters> Ledger<T, P> {
 
     /// Find the potential child block hashes given a parent block header.
     pub fn get_child_hashes(&self, parent_header: &BlockHeaderHash) -> Result<Vec<BlockHeaderHash>, StorageError> {
-        if !self.block_hash_exists(parent_header) {
-            return Ok(vec![]);
-        }
-
         match self.storage.get(COL_CHILD_HASHES, &parent_header.0)? {
             Some(encoded_child_block_hashes) => Ok(bincode::deserialize(&encoded_child_block_hashes[..])?),
             None => Ok(vec![]),
