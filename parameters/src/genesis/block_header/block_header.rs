4d9ebbfd8078301fd9eab255186697987956ba96
raychu86 <raychu86@gmail.com>
2020-06-15 21:18:10 -0700

    Add genesis implementation into parameters

---
+++ parameters/src/genesis/block_header/block_header.rs
@@ -0,0 +1,13 @@
+use snarkos_models::genesis::Genesis;
+
+pub struct GenesisBlockHeader;
+
+impl Genesis for GenesisBlockHeader {
+    const CHECKSUM: &'static str = "";
+    const SIZE: u64 = 503;
+
+    fn load_bytes() -> Vec<u8> {
+        let buffer = include_bytes!("block_header.genesis");
+        buffer.to_vec()
+    }
+}
