4ccaf2e3d3fce4cc1c4b48b85fbd7085974ffe39
raychu86 <raychu86@gmail.com>
2020-06-22 16:05:30 -0700

    Add temporary define merkle tree parameters macro

--- algorithms/src/merkle_tree/mod.rs
+++ algorithms/src/merkle_tree/mod.rs
@@ -64,3 +64,50 @@ macro_rules! define_merkle_tree_parameters {
         }
     };
 }
+
+// TODO (raychu86) Unify the macros - Currently failing because of duplicate imports when using `define_merkle_tree_parameters` twice in the same scope
+#[macro_export]
+/// Defines a Merkle Tree using the provided hash and height.
+macro_rules! define_merkle_tree_parameters_alternate {
+    ($struct_name:ident, $hash:ty, $height:expr) => {
+        use snarkos_models::algorithms::{MerkleParameters as OtherMerkleParameters, CRH as OtherCRH};
+        use $crate::merkle_tree::MerkleTree as OtherMerkleTree;
+
+        use rand::Rng as OtherRng;
+
+        #[derive(Clone, PartialEq, Eq, Debug)]
+        pub struct $struct_name($hash);
+
+        impl MerkleParameters for $struct_name {
+            type H = $hash;
+
+            const HEIGHT: usize = $height;
+
+            fn setup<R: OtherRng>(rng: &mut R) -> Self {
+                Self(Self::H::setup(rng))
+            }
+
+            fn crh(&self) -> &Self::H {
+                &self.0
+            }
+
+            fn parameters(&self) -> &<Self::H as OtherCRH>::Parameters {
+                self.crh().parameters()
+            }
+        }
+
+        impl Default for $struct_name {
+            fn default() -> Self {
+                Self(<Self as OtherMerkleParameters>::H::setup(
+                    &mut $crate::merkle_tree::prng(),
+                ))
+            }
+        }
+
+        impl From<$hash> for $struct_name {
+            fn from(crh: $hash) -> Self {
+                Self(crh)
+            }
+        }
+    };
+}
