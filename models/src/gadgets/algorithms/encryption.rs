486df27bdcd710e56729287ce1887048712d204b
raychu86 <raychu86@gmail.com>
2020-06-30 23:55:39 -0700

    Add encryption gadget functions

--- models/src/gadgets/algorithms/encryption.rs
+++ models/src/gadgets/algorithms/encryption.rs
@@ -3,7 +3,7 @@ use crate::{
     curves::Field,
     gadgets::{
         r1cs::ConstraintSystem,
-        utilities::{alloc::AllocGadget, eq::EqGadget, uint::UInt8, ToBytesGadget},
+        utilities::{alloc::AllocGadget, eq::EqGadget, ToBytesGadget},
     },
 };
 use snarkos_errors::gadgets::SynthesisError;
@@ -11,12 +11,23 @@ use snarkos_errors::gadgets::SynthesisError;
 use std::fmt::Debug;
 
 pub trait EncryptionGadget<E: EncryptionScheme, F: Field> {
-    type OutputGadget: EqGadget<F> + ToBytesGadget<F> + AllocGadget<E::Ciphertext, F> + Clone + Sized + Debug;
     type ParametersGadget: AllocGadget<E::Parameters, F> + Clone;
+    type PrivateKeyGadget: EqGadget<F> + ToBytesGadget<F> + AllocGadget<E::PrivateKey, F> + Clone + Sized + Debug;
+    type PublicKeyGadget: EqGadget<F> + ToBytesGadget<F> + AllocGadget<E::PublicKey, F> + Clone + Sized + Debug;
+    type CiphertextGadget: EqGadget<F> + ToBytesGadget<F> + AllocGadget<E::Ciphertext, F> + Clone + Sized + Debug;
+    type PlaintextGadget: EqGadget<F> + ToBytesGadget<F> + AllocGadget<E::Plaintext, F> + Clone + Sized + Debug;
 
     fn check_encryption_gadget<CS: ConstraintSystem<F>>(
         cs: CS,
         parameters: &Self::ParametersGadget,
-        input: &[UInt8],
-    ) -> Result<Self::OutputGadget, SynthesisError>;
+        public_key: &Self::PublicKeyGadget,
+        input: &Self::PlaintextGadget,
+    ) -> Result<Self::CiphertextGadget, SynthesisError>;
+
+    fn check_decryption_gadget<CS: ConstraintSystem<F>>(
+        cs: CS,
+        parameters: &Self::ParametersGadget,
+        private_key: &Self::PrivateKeyGadget,
+        input: &Self::CiphertextGadget,
+    ) -> Result<Self::PlaintextGadget, SynthesisError>;
 }
