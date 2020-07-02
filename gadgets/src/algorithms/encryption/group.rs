a15e508e1964f3b2143627eb28509bd5e339800e
raychu86 <raychu86@gmail.com>
2020-07-01 20:03:47 -0700

    Encryption gadget WIP

--- gadgets/src/algorithms/encryption/group.rs
+++ gadgets/src/algorithms/encryption/group.rs
@@ -4,13 +4,14 @@ use snarkos_models::{
     curves::{Field, Group, PrimeField, ProjectiveCurve},
     gadgets::{
         algorithms::EncryptionGadget,
-        curves::{CompressedGroupGadget, GroupGadget},
+        curves::{CompressedGroupGadget, FieldGadget, GroupGadget},
         r1cs::ConstraintSystem,
         utilities::{
             alloc::AllocGadget,
             boolean::Boolean,
             eq::{ConditionalEqGadget, EqGadget},
-            uint::UInt8,
+            uint::{UInt, UInt8},
+            ToBytesGadget,
         },
     },
 };
@@ -87,6 +88,74 @@ impl<G: Group, F: PrimeField> AllocGadget<G::ScalarField, F> for GroupEncryption
             UInt8::alloc_vec(cs, &private_key)?,
             PhantomData,
         ))
+    }
+}
+
+// GroupEncryption Randomness Gadget
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct GroupEncryptionRandomnessGadget<G: Group>(pub Vec<UInt8>, PhantomData<G>);
+
+impl<G: Group, F: PrimeField> AllocGadget<G::ScalarField, F> for GroupEncryptionRandomnessGadget<G> {
+    fn alloc<Fn: FnOnce() -> Result<T, SynthesisError>, T: Borrow<G::ScalarField>, CS: ConstraintSystem<F>>(
+        cs: CS,
+        value_gen: Fn,
+    ) -> Result<Self, SynthesisError> {
+        let randomness = to_bytes![value_gen()?.borrow()].unwrap();
+        Ok(GroupEncryptionRandomnessGadget(
+            UInt8::alloc_vec(cs, &randomness)?,
+            PhantomData,
+        ))
+    }
+
+    fn alloc_input<Fn: FnOnce() -> Result<T, SynthesisError>, T: Borrow<G::ScalarField>, CS: ConstraintSystem<F>>(
+        cs: CS,
+        value_gen: Fn,
+    ) -> Result<Self, SynthesisError> {
+        let randomness = to_bytes![value_gen()?.borrow()].unwrap();
+        Ok(GroupEncryptionRandomnessGadget(
+            UInt8::alloc_vec(cs, &randomness)?,
+            PhantomData,
+        ))
+    }
+}
+
+// GroupEncryption Blinding Exponents Gadget
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct GroupEncryptionBlindingExponentsGadget<G: Group>(pub Vec<Vec<UInt8>>, PhantomData<G>);
+
+impl<G: Group, F: PrimeField> AllocGadget<Vec<G::ScalarField>, F> for GroupEncryptionBlindingExponentsGadget<G> {
+    fn alloc<Fn: FnOnce() -> Result<T, SynthesisError>, T: Borrow<Vec<G::ScalarField>>, CS: ConstraintSystem<F>>(
+        mut cs: CS,
+        value_gen: Fn,
+    ) -> Result<Self, SynthesisError> {
+        let values = value_gen().map(|pp| pp.borrow().clone())?;
+
+        let mut blinding_exponents = Vec::with_capacity(values.len());
+        for (i, value) in values.into_iter().enumerate() {
+            let alloc = UInt8::alloc_vec(cs.ns(|| format!("Iteration {}", i)), &to_bytes![value.borrow()]?)?;
+            blinding_exponents.push(alloc);
+        }
+
+        Ok(GroupEncryptionBlindingExponentsGadget(blinding_exponents, PhantomData))
+    }
+
+    fn alloc_input<
+        Fn: FnOnce() -> Result<T, SynthesisError>,
+        T: Borrow<Vec<G::ScalarField>>,
+        CS: ConstraintSystem<F>,
+    >(
+        mut cs: CS,
+        value_gen: Fn,
+    ) -> Result<Self, SynthesisError> {
+        let values = value_gen().map(|pp| pp.borrow().clone())?;
+
+        let mut blinding_exponents = Vec::with_capacity(values.len());
+        for (i, value) in values.into_iter().enumerate() {
+            let alloc = UInt8::alloc_input_vec(cs.ns(|| format!("Iteration {}", i)), &to_bytes![value.borrow()]?)?;
+            blinding_exponents.push(alloc);
+        }
+
+        Ok(GroupEncryptionBlindingExponentsGadget(blinding_exponents, PhantomData))
     }
 }
 
@@ -223,7 +292,7 @@ impl<G: Group, F: Field, GG: GroupGadget<G, F>> EqGadget<F> for GroupEncryptionP
 // GroupEncryption Ciphertext Gadget
 #[derive(Debug, PartialEq, Eq)]
 pub struct GroupEncryptionCiphertextGadget<G: Group, F: Field, GG: GroupGadget<G, F>> {
-    plaintext: Vec<GG>,
+    ciphertext: Vec<GG>,
     _group: PhantomData<*const G>,
     _engine: PhantomData<*const F>,
 }
@@ -237,14 +306,14 @@ impl<G: Group + ProjectiveCurve, F: Field, GG: GroupGadget<G, F>> AllocGadget<Ve
     ) -> Result<Self, SynthesisError> {
         let values = f().map(|pp| pp.borrow().clone())?;
 
-        let mut plaintext = Vec::with_capacity(values.len());
+        let mut ciphertext = Vec::with_capacity(values.len());
         for (i, value) in values.into_iter().enumerate() {
             let alloc_group = GG::alloc_checked(cs.ns(|| format!("Iteration {}", i)), || Ok(value.borrow()))?;
-            plaintext.push(alloc_group);
+            ciphertext.push(alloc_group);
         }
 
         Ok(Self {
-            plaintext,
+            ciphertext,
             _engine: PhantomData,
             _group: PhantomData,
         })
@@ -256,14 +325,14 @@ impl<G: Group + ProjectiveCurve, F: Field, GG: GroupGadget<G, F>> AllocGadget<Ve
     ) -> Result<Self, SynthesisError> {
         let values = f().map(|pp| pp.borrow().clone())?;
 
-        let mut plaintext = Vec::with_capacity(values.len());
+        let mut ciphertext = Vec::with_capacity(values.len());
         for (i, value) in values.into_iter().enumerate() {
             let alloc_group = GG::alloc_input(cs.ns(|| format!("Iteration {}", i)), || Ok(value.borrow()))?;
-            plaintext.push(alloc_group);
+            ciphertext.push(alloc_group);
         }
 
         Ok(Self {
-            plaintext,
+            ciphertext,
             _engine: PhantomData,
             _group: PhantomData,
         })
@@ -273,7 +342,7 @@ impl<G: Group + ProjectiveCurve, F: Field, GG: GroupGadget<G, F>> AllocGadget<Ve
 impl<G: Group + ProjectiveCurve, F: Field, GG: GroupGadget<G, F>> Clone for GroupEncryptionCiphertextGadget<G, F, GG> {
     fn clone(&self) -> Self {
         Self {
-            plaintext: self.plaintext.clone(),
+            ciphertext: self.ciphertext.clone(),
             _group: PhantomData,
             _engine: PhantomData,
         }
@@ -288,7 +357,7 @@ impl<G: Group, F: Field, GG: GroupGadget<G, F>> ConditionalEqGadget<F> for Group
         other: &Self,
         condition: &Boolean,
     ) -> Result<(), SynthesisError> {
-        for (i, (m_i, other_m_i)) in self.plaintext.iter().zip(&other.plaintext).enumerate() {
+        for (i, (m_i, other_m_i)) in self.ciphertext.iter().zip(&other.ciphertext).enumerate() {
             m_i.conditional_enforce_equal(
                 &mut cs.ns(|| format!("conditional_enforce_equal index {}", i)),
                 &other_m_i,
@@ -309,36 +378,92 @@ impl<G: Group, F: Field, GG: GroupGadget<G, F>> EqGadget<F> for GroupEncryptionC
 // Group Encryption Gadget
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct GroupEncryptionGadget<G: Group + ProjectiveCurve, F: PrimeField, GG: CompressedGroupGadget<G, F>> {
+pub struct GroupEncryptionGadget<
+    G: Group + ProjectiveCurve,
+    F: PrimeField,
+    FG: FieldGadget<<G as Group>::ScalarField, F>,
+    GG: CompressedGroupGadget<G, F>,
+> {
+    _field_gadget: PhantomData<fn() -> FG>,
     _group: PhantomData<fn() -> G>,
     _group_gadget: PhantomData<fn() -> GG>,
     _engine: PhantomData<F>,
 }
 
-impl<F: PrimeField, G: Group + ProjectiveCurve, GG: CompressedGroupGadget<G, F>> EncryptionGadget<GroupEncryption<G>, F>
-    for GroupEncryptionGadget<G, F, GG>
+impl<
+    F: PrimeField,
+    G: Group + ProjectiveCurve,
+    FG: FieldGadget<<G as Group>::ScalarField, F>,
+    GG: CompressedGroupGadget<G, F>,
+> EncryptionGadget<GroupEncryption<G>, F> for GroupEncryptionGadget<G, F, FG, GG>
 {
+    type BlindingExponentGadget = GroupEncryptionBlindingExponentsGadget<G>;
     type CiphertextGadget = GroupEncryptionCiphertextGadget<G, F, GG>;
     type ParametersGadget = GroupEncryptionParametersGadget<G, F, GG>;
     type PlaintextGadget = GroupEncryptionPlaintextGadget<G, F, GG>;
     type PrivateKeyGadget = GroupEncryptionPrivateKeyGadget<G>;
     type PublicKeyGadget = GroupEncryptionPublicKeyGadget<G, F, GG>;
+    type RandomnessGadget = GroupEncryptionRandomnessGadget<G>;
 
-    //    fn check_encryption_gadget<CS: ConstraintSystem<F>>(
-    //        cs: CS,
-    //        parameters: &Self::ParametersGadget,
-    //        public_key: &Self::PublicKeyGadget,
-    //        input: &Self::PlaintextGadget,
-    //    ) -> Result<Self::CiphertextGadget, SynthesisError> {
-    //
-    //    }
+    fn check_encryption_gadget<CS: ConstraintSystem<F>>(
+        mut cs: CS,
+        parameters: &Self::ParametersGadget,               // g
+        randomness: &Self::RandomnessGadget,               // y
+        public_key: &Self::PublicKeyGadget,                // account_address
+        input: &Self::PlaintextGadget,                     // m
+        blinding_exponents: &Self::BlindingExponentGadget, // 1 [/] (z_i [+] j)
+    ) -> Result<(), SynthesisError> {
+        assert!(input.plaintext.len() > 0);
+        let zero = GG::zero(&mut cs.ns(|| "zero")).unwrap();
 
-    //    fn check_decryption_gadget<CS: ConstraintSystem<F>>(
-    //        cs: CS,
-    //        parameters: &Self::ParametersGadget,
-    //        private_key: &Self::PrivateKeyGadget,
-    //        input: &Self::CiphertextGadget,
-    //    ) -> Result<Self::PlaintextGadget, SynthesisError> {
-    //
-    //    }
+        let randomness_bits: Vec<_> = randomness.0.iter().flat_map(|byte| byte.clone().to_bits_le()).collect();
+        let c0 = parameters
+            .parameters
+            .mul_bits(cs.ns(|| "c_0"), &zero, randomness_bits.iter())?;
+
+        let record_view_key_gadget =
+            public_key
+                .public_key
+                .mul_bits(cs.ns(|| "shared_key"), &zero, randomness_bits.iter())?;
+
+        let z = record_view_key_gadget.to_x_coordinate();
+        let z_bytes = z.to_bytes(&mut cs.ns(|| "z_to_bytes"))?;
+        let z_bits: Vec<_> = z_bytes.iter().flat_map(|byte| byte.clone().to_bits_le()).collect();
+
+        let one = FG::one(cs.ns(|| "one"))?;
+        let mut j = FG::zero(cs.ns(|| "i"))?;
+
+        for (index, blinding_exponent) in blinding_exponents.0.iter().enumerate() {
+            let cs = &mut cs.ns(|| format!("iteration: {}", index));
+
+            let blinding_exponent_bits: Vec<_> = blinding_exponent
+                .iter()
+                .flat_map(|byte| byte.clone().to_bits_le())
+                .collect();
+            let h_j = record_view_key_gadget.mul_bits(
+                cs.ns(|| format!("h_{}", index)),
+                &zero,
+                blinding_exponent_bits.iter(),
+            )?;
+
+            // z * h_j
+            let a = h_j.mul_bits(cs.ns(|| format!("z * h_{}", index)), &zero, z_bits.iter())?;
+
+            // j * h_j
+            let j_bits = j.to_bits(cs.ns(|| format!("{}_bits", index)))?;
+            let b = h_j.mul_bits(cs.ns(|| format!("j * h_{}", index)), &zero, j_bits.iter())?;
+
+            // (z_i [+] j) * h_i,j
+            let expected_record_view_key = a.add(cs.ns(|| format!("expected record view key {}", index)), &b)?;
+
+            expected_record_view_key.enforce_equal(
+                &mut cs.ns(|| "Check that declared and computed record view keys are equal"),
+                &record_view_key_gadget,
+            )?;
+
+            j = j.add(cs.ns(|| format!("index {}", index)), &one)?;
+        }
+
+        Ok(())
+    }
 }
