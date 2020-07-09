d043fc6589196802f41844cdf4da46410f567063
raychu86 <raychu86@gmail.com>
2020-07-09 10:52:23 -0700

    formalize iterations

--- algorithms/src/encoding/tests.rs
+++ algorithms/src/encoding/tests.rs
@@ -6,11 +6,13 @@ use snarkos_utilities::rand::UniformRand;
 use rand::SeedableRng;
 use rand_xorshift::XorShiftRng;
 
+pub(crate) const ITERATIONS: usize = 10000;
+
 #[test]
 fn test_elligator2_encoding() {
     let rng = &mut XorShiftRng::seed_from_u64(1231275789u64);
 
-    for _ in 0..10000 {
+    for _ in 0..ITERATIONS {
         let fr_element: Fr = Fr::rand(rng);
 
         let encoded_element = Elligator2::<EdwardsParameters, EdwardsProjective>::encode(&fr_element).unwrap();
