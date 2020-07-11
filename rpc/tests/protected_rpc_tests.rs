54c11c75813e777c1ae6a28066c3c07956bfdf14
howardwu <howardwu@berkeley.edu>
2020-07-11 14:27:10 -0700

    Fix create account rpc

--- rpc/tests/protected_rpc_tests.rs
+++ rpc/tests/protected_rpc_tests.rs
@@ -17,7 +17,6 @@ mod protected_rpc_tests {
     };
 
     use jsonrpc_core::MetaIoHandler;
-    use rand::Rng;
     use serde_json::Value;
     use std::{str::FromStr, sync::Arc};
     use tokio::sync::Mutex;
@@ -215,13 +214,10 @@ mod protected_rpc_tests {
     fn test_create_account() {
         let storage = Arc::new(FIXTURE_VK.ledger());
         let parameters = load_verifying_parameters();
-        let mut rng = FIXTURE_VK.rng.clone();
         let meta = authentication();
         let rpc = initialize_test_rpc(&storage, parameters);
 
         let method = "createaccount".to_string();
-        let metadata: [u8; 32] = rng.gen();
-        let params = hex::encode(metadata);
 
         // Request without specified metadata
         let request_without_metadata = format!("{{ \"jsonrpc\":\"2.0\", \"id\": 1, \"method\": \"{}\" }}", method);
@@ -236,12 +232,9 @@ mod protected_rpc_tests {
         let _private_key = AccountPrivateKey::<Components>::from_str(&account.private_key).unwrap();
         let _address = AccountAddress::<Components>::from_str(&account.address).unwrap();
 
-        // Request with specified metadata
-        let request_with_metadata = format!(
-            "{{ \"jsonrpc\":\"2.0\", \"id\": 1, \"method\": \"{}\", \"params\": [\"{}\"] }}",
-            method, params
-        );
-        let response = rpc.handle_request_sync(&request_with_metadata, meta).unwrap();
+        // Request without specified metadata
+        let request_without_metadata = format!("{{ \"jsonrpc\":\"2.0\", \"id\": 1, \"method\": \"{}\" }}", method);
+        let response = rpc.handle_request_sync(&request_without_metadata, meta).unwrap();
 
         let extracted: Value = serde_json::from_str(&response).unwrap();
 
