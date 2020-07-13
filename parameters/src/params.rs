96614c98729a81081889825b6c53038a726f36bc
howardwu <howard@aleo.org>
2020-07-13 20:27:37 +0000

    Fix parameter sizes

--- parameters/src/params.rs
+++ parameters/src/params.rs
@@ -255,8 +255,8 @@ impl_params!(
 
 // Inner SNARK
 impl_params_remote!(InnerSNARKPKParameters, "inner_snark_pk", 210199697);
-impl_params!(InnerSNARKVKParameters, inner_snark_vk_test, "inner_snark_vk", 2426);
+impl_params!(InnerSNARKVKParameters, inner_snark_vk_test, "inner_snark_vk", 2232);
 
 // Outer SNARK
 impl_params_remote!(OuterSNARKPKParameters, "outer_snark_pk", 467030109);
-impl_params!(OuterSNARKVKParameters, outer_snark_vk_test, "outer_snark_vk", 4443);
+impl_params!(OuterSNARKVKParameters, outer_snark_vk_test, "outer_snark_vk", 4057);
