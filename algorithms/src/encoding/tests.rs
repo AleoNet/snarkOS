549ca8b7e5773e8ee4a84635ace79a57a23e05ec
raychu86 <raychu86@gmail.com>
2020-07-14 12:40:00 -0700

    Clean up elligator2 test

--- algorithms/src/encoding/tests.rs
+++ algorithms/src/encoding/tests.rs
@@ -6,7 +6,7 @@ use snarkos_utilities::rand::UniformRand;
 use rand::SeedableRng;
 use rand_xorshift::XorShiftRng;
 
-pub(crate) const ITERATIONS: usize = 5;
+pub(crate) const ITERATIONS: usize = 10000;
 
 #[test]
 fn test_elligator2_encode_decode() {
@@ -17,40 +17,6 @@ fn test_elligator2_encode_decode() {
 
         let (encoded, fq_high) = Elligator2::<EdwardsParameters, EdwardsProjective>::encode(&original).unwrap();
         let decoded = Elligator2::<EdwardsParameters, EdwardsProjective>::decode(&encoded, fq_high).unwrap();
-
-        //        println!("\n{} == {}", original, decoded);
-
-        use snarkos_models::curves::{AffineCurve, ProjectiveCurve};
-        let encoded_x = encoded.x;
-
-        println!("encoded: {:?}", encoded);
-
-        //        let x_bytes = to_bytes![encoded_x].unwrap();
-
-        if let Some(affine) = <EdwardsProjective as ProjectiveCurve>::Affine::from_x_coordinate(encoded_x.clone(), true)
-        {
-            if affine.is_in_correct_subgroup_assuming_on_curve() {
-                println!("affine 1: {:?}", affine);
-            }
-        }
-
-        if let Some(affine) =
-            <EdwardsProjective as ProjectiveCurve>::Affine::from_x_coordinate(encoded_x.clone(), false)
-        {
-            if affine.is_in_correct_subgroup_assuming_on_curve() {
-                println!("affine 1: {:?}", affine);
-            }
-        }
-
-        //
-        //        let greatest = match <EdwardsProjective as ProjectiveCurve>::Affine::from_x_coordinate(encoded_x.clone(), true) {
-        //            Some(affine) => encoded == affine,
-        //            None => false,
-        //        };
-        //
-        //        let recovered = <EdwardsProjective as ProjectiveCurve>::Affine::from_x_coordinate(encoded_x, greatest).unwrap();
-
-        assert_eq!(encoded, recovered);
 
         assert_eq!(original, decoded)
     }
