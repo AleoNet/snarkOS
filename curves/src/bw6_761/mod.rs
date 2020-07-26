4322c9e89c3de6520504e9db77cfd68541bf68da
howardwu <howardwu@berkeley.edu>
2020-07-26 14:57:29 -0700

    Adds rustdocs for snarkos-curves

--- curves/src/bw6_761/mod.rs
+++ curves/src/bw6_761/mod.rs
@@ -1,22 +1,31 @@
+#![cfg_attr(nightly, doc(include = "../../documentation/the_aleo_curves/04_bw6-761.md"))]
+
 pub mod fr;
+#[doc(inline)]
 pub use self::fr::*;
 
 pub mod fq;
+#[doc(inline)]
 pub use self::fq::*;
 
 pub mod fq3;
+#[doc(inline)]
 pub use self::fq3::*;
 
 pub mod fq6;
+#[doc(inline)]
 pub use self::fq6::*;
 
 pub mod g1;
+#[doc(inline)]
 pub use self::g1::*;
 
 pub mod g2;
+#[doc(inline)]
 pub use self::g2::*;
 
 pub mod parameters;
+#[doc(inline)]
 pub use self::parameters::*;
 
 #[cfg(test)]
