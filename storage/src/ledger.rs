8aaa3589849db5587c40a07b65574d5c1d31373b
raychu86 <raychu86@gmail.com>
2020-09-13 00:02:21 -0700

    Fix comments

--- storage/src/ledger.rs
+++ storage/src/ledger.rs
@@ -166,12 +166,14 @@ impl<T: Transaction, P: LoadableMerkleParameters> Ledger<T, P> {
     pub fn catch_up_secondary(&self) -> Result<(), StorageError> {
         // Sync the secondary and primary instances
         if self.storage.db.try_catch_up_with_primary().is_ok() {
-            // Update the latest block height of the secondary instance.
             let latest_block_height_bytes = self.get(COL_META, &KEY_BEST_BLOCK_NUMBER.as_bytes().to_vec())?;
             let new_latest_block_height = bytes_to_u32(latest_block_height_bytes);
             let mut latest_block_height = self.latest_block_height.write();
 
+            // If the new block height is greater than the stored block height,
+            // update the block height and merkle tree.
             if new_latest_block_height > *latest_block_height {
+                // Update the latest block height of the secondary instance.
                 *latest_block_height = new_latest_block_height;
 
                 // Update the Merkle tree of the secondary instance.
