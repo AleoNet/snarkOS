440b4c1409a2a04cc813894a4c62fd7a93a3bee4
raychu86 <raychu86@gmail.com>
2020-09-16 20:10:01 -0700

    Migrate connection_handler to inbound

--- network/src/inbound/mod.rs
+++ network/src/inbound/mod.rs
@@ -13,3 +13,6 @@
 
 // You should have received a copy of the GNU General Public License
 // along with the snarkOS library. If not, see <https://www.gnu.org/licenses/>.
+
+pub mod connection_handler;
+pub use connection_handler::*;
