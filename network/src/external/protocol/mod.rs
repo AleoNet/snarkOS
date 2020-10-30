0ae11800f71be0773973b50170ba3e258c0b5a81
howardwu <howardwu@berkeley.edu>
2020-10-30 00:19:44 -0700

    Drops Handshakes from network

--- network/src/external/protocol/mod.rs
+++ network/src/external/protocol/mod.rs
@@ -14,9 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkOS library. If not, see <https://www.gnu.org/licenses/>.
 
-pub mod handshake;
-pub use handshake::*;
-
 pub mod ping_pong_manager;
 pub use ping_pong_manager::*;
 
