fe764336e5375871d02394150ae3cba05a72e6fc
raychu86 <raychu86@gmail.com>
2020-11-18 19:27:25 -0800

    Use compressed serialization and deserialization for gm17 parameters

--- algorithms/src/snark/gm17/mod.rs
+++ algorithms/src/snark/gm17/mod.rs
@@ -18,10 +18,10 @@
 //! [GM17]: https://eprint.iacr.org/2017/540
 
 use snarkos_errors::{gadgets::SynthesisResult, serialization::SerializationError};
-use snarkos_models::curves::pairing_engine::{AffineCurve, PairingCurve, PairingEngine};
+use snarkos_models::curves::pairing_engine::{PairingCurve, PairingEngine};
 use snarkos_utilities::{serialize::*, FromBytes, ToBytes};
 
-use std::io::{self, Read, Result as IoResult, Write};
+use std::io::{Read, Result as IoResult, Write};
 
 /// GM17 zkSNARK construction.
 pub mod snark;
@@ -100,7 +100,7 @@ impl<E: PairingEngine> Proof<E> {
 }
 
 /// A verification key in the GM17 SNARK.
-#[derive(Clone, Debug, Eq)]
+#[derive(Clone, Debug, Eq, CanonicalSerialize, CanonicalDeserialize)]
 pub struct VerifyingKey<E: PairingEngine> {
     pub h_g2: E::G2Affine,
     pub g_alpha_g1: E::G1Affine,
@@ -190,7 +190,7 @@ impl<E: PairingEngine> VerifyingKey<E> {
 }
 
 /// Full public (prover and verifier) parameters for the GM17 zkSNARK.
-#[derive(Clone, Debug, Eq)]
+#[derive(Clone, Debug, Eq, CanonicalSerialize, CanonicalDeserialize)]
 pub struct Parameters<E: PairingEngine> {
     pub vk: VerifyingKey<E>,
     pub a_query: Vec<E::G1Affine>,
@@ -228,7 +228,7 @@ impl<E: PairingEngine> ToBytes for Parameters<E> {
 impl<E: PairingEngine> FromBytes for Parameters<E> {
     #[inline]
     fn read<R: Read>(mut reader: R) -> IoResult<Self> {
-        Self::read(&mut reader, false)
+        Self::read(&mut reader)
     }
 }
 
@@ -247,121 +247,14 @@ impl<E: PairingEngine> From<Parameters<E>> for PreparedVerifyingKey<E> {
 impl<E: PairingEngine> Parameters<E> {
     /// Serialize the parameters to bytes.
     pub fn write<W: Write>(&self, mut writer: W) -> IoResult<()> {
-        self.vk.write(&mut writer)?;
-
-        (self.a_query.len() as u32).write(&mut writer)?;
-        for g in &self.a_query[..] {
-            g.write(&mut writer)?;
-        }
-
-        (self.b_query.len() as u32).write(&mut writer)?;
-        for g in &self.b_query[..] {
-            g.write(&mut writer)?;
-        }
-
-        (self.c_query_1.len() as u32).write(&mut writer)?;
-        for g in &self.c_query_1[..] {
-            g.write(&mut writer)?;
-        }
-
-        (self.c_query_2.len() as u32).write(&mut writer)?;
-        for g in &self.c_query_2[..] {
-            g.write(&mut writer)?;
-        }
-
-        self.g_gamma_z.write(&mut writer)?;
-
-        self.h_gamma_z.write(&mut writer)?;
-
-        self.g_ab_gamma_z.write(&mut writer)?;
-
-        self.g_gamma2_z2.write(&mut writer)?;
-
-        (self.g_gamma2_z_t.len() as u32).write(&mut writer)?;
-        for g in &self.g_gamma2_z_t[..] {
-            g.write(&mut writer)?;
-        }
+        CanonicalSerialize::serialize(self, &mut writer)?;
 
         Ok(())
     }
 
     /// Deserialize the public parameters from bytes.
-    pub fn read<R: Read>(mut reader: R, checked: bool) -> IoResult<Self> {
-        let read_g1_affine = |mut reader: &mut R| -> IoResult<E::G1Affine> {
-            let g1_affine: E::G1Affine = FromBytes::read(&mut reader)?;
-
-            if checked && !g1_affine.is_in_correct_subgroup_assuming_on_curve() {
-                return Err(io::Error::new(
-                    io::ErrorKind::InvalidData,
-                    "point is not in the correct subgroup",
-                ));
-            }
-
-            Ok(g1_affine)
-        };
-
-        let read_g2_affine = |mut reader: &mut R| -> IoResult<E::G2Affine> {
-            let g2_affine: E::G2Affine = FromBytes::read(&mut reader)?;
-
-            if checked && !g2_affine.is_in_correct_subgroup_assuming_on_curve() {
-                return Err(io::Error::new(
-                    io::ErrorKind::InvalidData,
-                    "point is not in the correct subgroup",
-                ));
-            }
-
-            Ok(g2_affine)
-        };
-
-        let vk = VerifyingKey::<E>::read(&mut reader)?;
-
-        let a_query_len: u32 = FromBytes::read(&mut reader)?;
-        let mut a_query = Vec::with_capacity(a_query_len as usize);
-        for _ in 0..a_query_len {
-            a_query.push(read_g1_affine(&mut reader)?);
-        }
-
-        let b_query_len: u32 = FromBytes::read(&mut reader)?;
-        let mut b_query = Vec::with_capacity(b_query_len as usize);
-        for _ in 0..b_query_len {
-            b_query.push(read_g2_affine(&mut reader)?);
-        }
-
-        let c_query_1_len: u32 = FromBytes::read(&mut reader)?;
-        let mut c_query_1 = Vec::with_capacity(c_query_1_len as usize);
-        for _ in 0..c_query_1_len {
-            c_query_1.push(read_g1_affine(&mut reader)?);
-        }
-
-        let c_query_2_len: u32 = FromBytes::read(&mut reader)?;
-        let mut c_query_2 = Vec::with_capacity(c_query_2_len as usize);
-        for _ in 0..c_query_2_len {
-            c_query_2.push(read_g1_affine(&mut reader)?);
-        }
-
-        let g_gamma_z: E::G1Affine = FromBytes::read(&mut reader)?;
-        let h_gamma_z: E::G2Affine = FromBytes::read(&mut reader)?;
-        let g_ab_gamma_z: E::G1Affine = FromBytes::read(&mut reader)?;
-        let g_gamma2_z2: E::G1Affine = FromBytes::read(&mut reader)?;
-
-        let g_gamma2_z_t_len: u32 = FromBytes::read(&mut reader)?;
-        let mut g_gamma2_z_t = Vec::with_capacity(g_gamma2_z_t_len as usize);
-        for _ in 0..g_gamma2_z_t_len {
-            g_gamma2_z_t.push(read_g1_affine(&mut reader)?);
-        }
-
-        Ok(Self {
-            vk,
-            a_query,
-            b_query,
-            c_query_1,
-            c_query_2,
-            g_gamma_z,
-            h_gamma_z,
-            g_ab_gamma_z,
-            g_gamma2_z2,
-            g_gamma2_z_t,
-        })
+    pub fn read<R: Read>(mut reader: R) -> IoResult<Self> {
+        Ok(CanonicalDeserialize::deserialize(&mut reader)?)
     }
 }
 
