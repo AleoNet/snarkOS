cec7aa998681cc839e669877d3997dcb131e2100
ljedrz <ljedrz@gmail.com>
2020-12-04 12:55:34 +0100

    refactor(models): deduplicate an AllocGadget impl
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- models/src/gadgets/utilities/int/utilities/alloc.rs
+++ models/src/gadgets/utilities/int/utilities/alloc.rs
@@ -32,88 +32,55 @@ use snarkos_errors::gadgets::SynthesisError;
 
 use core::borrow::Borrow;
 
+macro_rules! alloc_int_fn_impl {
+    ($gadget: ident, $fn_name: ident) => {
+        fn $fn_name<
+            Fn: FnOnce() -> Result<T, SynthesisError>,
+            T: Borrow<<$gadget as Int>::IntegerType>,
+            CS: ConstraintSystem<F>,
+        >(
+            mut cs: CS,
+            value_gen: Fn,
+        ) -> Result<Self, SynthesisError> {
+            let value = value_gen().map(|val| *val.borrow());
+            let values = match value {
+                Ok(mut val) => {
+                    let mut v = Vec::with_capacity(<$gadget as Int>::SIZE);
+
+                    for _ in 0..<$gadget as Int>::SIZE {
+                        v.push(Some(val & 1 == 1));
+                        val >>= 1;
+                    }
+
+                    v
+                }
+                _ => vec![None; <$gadget as Int>::SIZE],
+            };
+
+            let bits = values
+                .into_iter()
+                .enumerate()
+                .map(|(i, v)| {
+                    Ok(Boolean::from(AllocatedBit::$fn_name(
+                        &mut cs.ns(|| format!("allocated bit_gadget {}", i)),
+                        || v.ok_or(SynthesisError::AssignmentMissing),
+                    )?))
+                })
+                .collect::<Result<Vec<_>, SynthesisError>>()?;
+
+            Ok(Self {
+                bits,
+                value: value.ok(),
+            })
+        }
+    };
+}
+
 macro_rules! alloc_int_impl {
     ($($gadget: ident)*) => ($(
         impl<F: Field> AllocGadget<<$gadget as Int>::IntegerType, F> for $gadget {
-            fn alloc<
-                Fn: FnOnce() -> Result<T, SynthesisError>,
-                T: Borrow<<$gadget as Int>::IntegerType>,
-                CS: ConstraintSystem<F>
-            >(
-                mut cs: CS,
-                value_gen: Fn,
-            ) -> Result<Self, SynthesisError> {
-                let value = value_gen().map(|val| *val.borrow());
-                let values = match value {
-                    Ok(mut val) => {
-                        let mut v = Vec::with_capacity(<$gadget as Int>::SIZE);
-
-                        for _ in 0..<$gadget as Int>::SIZE {
-                            v.push(Some(val & 1 == 1));
-                            val >>= 1;
-                        }
-
-                        v
-                    }
-                    _ => vec![None; <$gadget as Int>::SIZE],
-                };
-
-                let bits = values
-                    .into_iter()
-                    .enumerate()
-                    .map(|(i, v)| {
-                        Ok(Boolean::from(AllocatedBit::alloc(
-                            &mut cs.ns(|| format!("allocated bit_gadget {}", i)),
-                            || v.ok_or(SynthesisError::AssignmentMissing),
-                        )?))
-                    })
-                    .collect::<Result<Vec<_>, SynthesisError>>()?;
-
-                Ok(Self {
-                    bits,
-                    value: value.ok(),
-                })
-            }
-
-            fn alloc_input<
-                Fn: FnOnce() -> Result<T, SynthesisError>,
-                T: Borrow<<$gadget as Int>::IntegerType>,
-                CS: ConstraintSystem<F>
-            >(
-                mut cs: CS,
-                value_gen: Fn,
-            ) -> Result<Self, SynthesisError> {
-                let value = value_gen().map(|val| *val.borrow());
-                let values = match value {
-                    Ok(mut val) => {
-                        let mut v = Vec::with_capacity(<$gadget as Int>::SIZE);
-
-                        for _ in 0..<$gadget as Int>::SIZE {
-                            v.push(Some(val & 1 == 1));
-                            val >>= 1;
-                        }
-
-                        v
-                    }
-                    _ => vec![None; <$gadget as Int>::SIZE],
-                };
-
-                let bits = values
-                    .into_iter()
-                    .enumerate()
-                    .map(|(i, v)| {
-                        Ok(Boolean::from(AllocatedBit::alloc_input(
-                            &mut cs.ns(|| format!("allocated bit_gadget {}", i)),
-                            || v.ok_or(SynthesisError::AssignmentMissing),
-                        )?))
-                    })
-                    .collect::<Result<Vec<_>, SynthesisError>>()?;
-
-                Ok(Self {
-                    bits,
-                    value: value.ok(),
-                })
-            }
+            alloc_int_fn_impl!($gadget, alloc);
+            alloc_int_fn_impl!($gadget, alloc_input);
         }
     )*)
 }
