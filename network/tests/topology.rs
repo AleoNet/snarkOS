6442ee45ef9636c89166ac9e6dc094533e10b413
Niklas <niklas@long.ch>
2021-01-26 11:29:33 +0100

    test(network): add topology tests

--- network/tests/topology.rs
+++ network/tests/topology.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2019-2020 Aleo Systems Inc.
+// Copyright (C) 2019-2021 Aleo Systems Inc.
 // This file is part of the snarkOS library.
 
 // The snarkOS library is free software: you can redistribute it and/or modify
@@ -17,12 +17,47 @@
 use snarkos_network::external::message::*;
 use snarkos_testing::{
     network::{
+        connect_nodes,
         handshaken_node_and_peer,
         random_bound_address,
         read_header,
         read_payload,
         write_message_to_stream,
         TestSetup,
+        Topology,
     },
     wait_until,
 };
+
+#[tokio::test]
+async fn line() {
+    let setup = TestSetup {
+        consensus_setup: None,
+        peer_sync_interval: 2,
+        ..Default::default()
+    };
+    let nodes = connect_nodes(10, setup, Topology::Line).await;
+
+    // First and Last nodes should have 1 connected peer.
+    wait_until!(5, nodes.first().unwrap().peers.number_of_connected_peers() == 1);
+    assert_eq!(nodes.last().unwrap().peers.number_of_connected_peers(), 1);
+
+    // All other nodes should have two.
+    for i in 1..(nodes.len() - 1) {
+        assert_eq!(nodes[i].peers.number_of_connected_peers(), 2);
+    }
+}
+
+#[tokio::test]
+async fn star() {
+    let setup = TestSetup {
+        consensus_setup: None,
+        peer_sync_interval: 2,
+        ..Default::default()
+    };
+    let nodes = connect_nodes(10, setup, Topology::Star).await;
+    let core = nodes.first().unwrap();
+
+    assert!(core.environment.is_bootnode());
+    wait_until!(5, core.peers.number_of_connected_peers() == 9);
+}
