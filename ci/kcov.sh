fdfccbc08b99f79d82fbe833bc0358748133ce45
howardwu <howardwu@berkeley.edu>
2021-01-28 21:09:47 -0800

    Debug

--- ci/kcov.sh
+++ ci/kcov.sh
@@ -1,5 +1,11 @@
 cd /home/circleci/project/ &&
 rm -rf target/debug/base_dpc* && rm -rf target/debug/consensus_dpc* && rm -rf target/debug/consensus_integration* && rm -rf target/debug/miner* && rm -rf target/debug/protected_rpc_tests*
+ls target
+echo "-1-"
+ls target/debug
+echo "-2-"
+ls target/debug/deps
+echo "-3-"
 for file in target/debug/*-*[^\.d];
   do
     mkdir -p "target/cov/$(basename $file)";
