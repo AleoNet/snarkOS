1596e15a72f32d2d38c1eb8e24d71979713ac95d
howardwu <howard@aleo.org>
2021-04-27 03:46:36 -0700

    use filter nit

--- network/src/peers/peers.rs
+++ network/src/peers/peers.rs
@@ -67,13 +67,9 @@ impl<S: Storage + Send + Sync + 'static> Node<S> {
             .peer_book
             .connected_peers()
             .iter()
+            .filter(|(addr, _)| !bootnodes.contains(addr)) // Skip this check if the peer is a bootnode.
             .map(|(addr, info)| (*addr, &info.quality))
         {
-            // Skip this check if the peer is a bootnode.
-            if bootnodes.contains(&addr) {
-                continue;
-            }
-
             if peer_quality.rtt_ms.load(Ordering::Relaxed) > 1000 || peer_quality.failures.load(Ordering::Relaxed) > 10
             {
                 warn!("Peer {} has a low quality score; disconnecting.", addr);
