5ae7ec53c48b36e882c77c5b1396b3e32355dc24
Sam De Roeck <sadroeck@gmail.com>
2021-06-18 12:25:58 -0400

    [fix] Add missing license text

--- metrics/src/names.rs
+++ metrics/src/names.rs
@@ -1,3 +1,19 @@
+// Copyright (C) 2019-2021 Aleo Systems Inc.
+// This file is part of the snarkOS library.
+
+// The snarkOS library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The snarkOS library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with the snarkOS library. If not, see <https://www.gnu.org/licenses/>.
+
 pub mod inbound {
     pub const ALL_SUCCESSES: &str = "snarkos_inbound_all_successes_total";
     pub const ALL_FAILURES: &str = "snarkos_inbound_all_failures_total";
