20b0ff6578ec83b36a44ca147e30be48129ade56
ljedrz <ljedrz@gmail.com>
2021-06-21 15:31:58 +0200

    perf: validate block transactions in parallel
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- storage/src/validator.rs
+++ storage/src/validator.rs
@@ -27,23 +27,31 @@ use snarkvm_algorithms::traits::LoadableMerkleParameters;
 use snarkvm_dpc::{Block, BlockHeaderHash, DatabaseTransaction, LedgerScheme, Op, Storage, TransactionScheme};
 use snarkvm_utilities::{to_bytes, ToBytes};
 
+use parking_lot::Mutex;
+use rayon::prelude::*;
 use tracing::*;
 
-use std::collections::HashSet;
+use std::{
+    collections::HashSet,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+};
 
 macro_rules! check_for_superfluous_tx_components {
     ($fn_name:ident, $component_name:expr, $component_col:expr) => {
         fn $fn_name(
             &self,
             tx_entries: &HashSet<Vec<u8>>,
-            db_ops: &mut Option<DatabaseTransaction>,
-            is_storage_valid: &mut bool,
+            db_ops: &Mutex<Option<DatabaseTransaction>>,
+            is_storage_valid: &AtomicBool,
         ) {
             let storage_entries_and_indices = match self.storage.get_col($component_col) {
                 Ok(col) => col,
                 Err(e) => {
                     error!("Couldn't obtain the column with tx {}s: {}", $component_name, e);
-                    *is_storage_valid = false;
+                    is_storage_valid.store(false, Ordering::SeqCst);
 
                     return;
                 }
@@ -63,7 +71,7 @@ macro_rules! check_for_superfluous_tx_components {
                     $component_name
                 );
 
-                if let Some(ref mut ops) = db_ops {
+                if let Some(ref mut ops) = &mut *db_ops.lock() {
                     for superfluous_item in superfluous_items {
                         trace!("Staging a {} for deletion", $component_name);
                         ops.push(Op::Delete {
@@ -72,7 +80,7 @@ macro_rules! check_for_superfluous_tx_components {
                         });
                     }
                 } else {
-                    *is_storage_valid = false;
+                    is_storage_valid.store(false, Ordering::SeqCst);
                 }
             }
         }
@@ -93,7 +101,7 @@ pub enum FixMode {
     Everything,
 }
 
-impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P, S> {
+impl<T: TransactionScheme + Send + Sync, P: LoadableMerkleParameters, S: Storage + Sync> Ledger<T, P, S> {
     check_for_superfluous_tx_components!(check_for_superfluous_tx_memos, "memorandums", COL_MEMO);
 
     check_for_superfluous_tx_components!(check_for_superfluous_tx_digests, "digests", COL_DIGEST);
@@ -116,18 +124,18 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
 
         info!("Validating the storage...");
 
-        let mut is_valid = true;
+        let is_valid = AtomicBool::new(true);
 
         if limit == Some(0) {
             info!("The limit of blocks to validate is 0; nothing to check.");
 
-            return is_valid;
+            return is_valid.load(Ordering::SeqCst);
         }
 
-        let mut db_ops = if fix_mode != FixMode::Nothing {
-            Some(DatabaseTransaction::new())
+        let db_ops = if fix_mode != FixMode::Nothing {
+            Arc::new(Mutex::new(Some(DatabaseTransaction::new())))
         } else {
-            None
+            Arc::new(Mutex::new(None))
         };
 
         let mut current_height = self.get_current_block_height();
@@ -135,20 +143,20 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
         if current_height == 0 {
             info!("Only the genesis block is currently available; nothing to check.");
 
-            return is_valid;
+            return is_valid.load(Ordering::SeqCst);
         }
 
         debug!("The block height is {}", current_height);
 
         match self.get_best_block_number() {
             Err(_) => {
-                is_valid = false;
+                is_valid.store(false, Ordering::SeqCst);
                 error!("Can't obtain the best block number from storage!");
             }
             Ok(number) => {
                 // Initial block height comes from KEY_BEST_BLOCK_NUMBER.
                 if number != current_height {
-                    is_valid = false;
+                    is_valid.store(false, Ordering::SeqCst);
                     error!("Current best block number doesn't match the block height!");
                 }
             }
@@ -159,7 +167,7 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
         // Current block is found by COL_BLOCK_LOCATOR, as it should have been committed (i.e. be canon).
         let mut current_block = self.get_block_from_block_number(current_height);
         while let Err(e) = current_block {
-            is_valid = false;
+            is_valid.store(false, Ordering::SeqCst);
             error!(
                 "Couldn't find the latest block (height {}): {}! Trying a lower height next.",
                 current_height, e
@@ -174,7 +182,7 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
                 if *limit == 0 {
                     info!("Specified block limit reached; the check is complete.");
 
-                    return is_valid;
+                    return is_valid.load(Ordering::SeqCst);
                 }
             }
 
@@ -195,10 +203,10 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
             debug!("The true block height is {}", current_height);
         }
 
-        let mut tx_memos = HashSet::new();
-        let mut tx_sns = HashSet::new();
-        let mut tx_cms = HashSet::new();
-        let mut tx_digests = HashSet::new();
+        let tx_memos = Default::default();
+        let tx_sns = Default::default();
+        let tx_cms = Default::default();
+        let tx_digests = Default::default();
 
         let mut current_hash = current_block.header.get_hash();
 
@@ -210,20 +218,20 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
             }
 
             if !self.block_hash_exists(&current_hash) {
-                is_valid = false;
+                is_valid.store(false, Ordering::SeqCst);
                 error!("The header for block at height {} is missing!", current_height);
             }
 
             self.validate_block_transactions(
                 &current_block,
                 current_height,
-                &mut tx_memos,
-                &mut tx_sns,
-                &mut tx_cms,
-                &mut tx_digests,
-                &mut db_ops,
+                &tx_memos,
+                &tx_sns,
+                &tx_cms,
+                &tx_digests,
+                &db_ops,
                 fix_mode,
-                &mut is_valid,
+                &is_valid,
             );
 
             if current_height == 0 {
@@ -245,7 +253,7 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
             let previous_hash = previous_block.header.get_hash();
 
             if current_block.header.previous_block_hash != previous_hash {
-                is_valid = false;
+                is_valid.store(false, Ordering::SeqCst);
                 error!(
                     "The parent hash of block at height {} doesn't match its child at {}!",
                     current_height + 1,
@@ -255,12 +263,12 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
 
             match self.get_child_block_hashes(&previous_hash) {
                 Err(e) => {
-                    is_valid = false;
+                    is_valid.store(false, Ordering::SeqCst);
                     error!("Can't find the children of block at height {}: {}!", previous_hash, e);
                 }
                 Ok(child_hashes) => {
                     if !child_hashes.contains(&current_hash) {
-                        is_valid = false;
+                        is_valid.store(false, Ordering::SeqCst);
                         error!(
                             "The list of children hash of block at height {} don't contain the child at {}!",
                             current_height,
@@ -284,13 +292,13 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
         }
 
         if [FixMode::SuperfluousTxComponents, FixMode::Everything].contains(&fix_mode) {
-            self.check_for_superfluous_tx_memos(&tx_memos, &mut db_ops, &mut is_valid);
-            self.check_for_superfluous_tx_digests(&tx_digests, &mut db_ops, &mut is_valid);
-            self.check_for_superfluous_tx_sns(&tx_sns, &mut db_ops, &mut is_valid);
-            self.check_for_superfluous_tx_cms(&tx_cms, &mut db_ops, &mut is_valid);
+            self.check_for_superfluous_tx_memos(&*tx_memos.lock(), &db_ops, &is_valid);
+            self.check_for_superfluous_tx_digests(&*tx_digests.lock(), &db_ops, &is_valid);
+            self.check_for_superfluous_tx_sns(&*tx_sns.lock(), &db_ops, &is_valid);
+            self.check_for_superfluous_tx_cms(&*tx_cms.lock(), &db_ops, &is_valid);
         }
 
-        if let Some(ops) = db_ops {
+        if let Some(ops) = db_ops.lock().take() {
             if !ops.0.is_empty() {
                 info!("Fixing the detected storage issues ({} fixes)", ops.0.len());
                 if let Err(e) = self.storage.batch(ops) {
@@ -298,6 +306,8 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
                 }
             }
         }
+
+        let is_valid = is_valid.load(Ordering::SeqCst);
 
         if is_valid {
             info!("The storage is valid!");
@@ -313,15 +323,15 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
         &self,
         block: &Block<T>,
         block_height: u32,
-        tx_memos: &mut HashSet<Vec<u8>>,
-        tx_sns: &mut HashSet<Vec<u8>>,
-        tx_cms: &mut HashSet<Vec<u8>>,
-        tx_digests: &mut HashSet<Vec<u8>>,
-        database_fix: &mut Option<DatabaseTransaction>,
+        tx_memos: &Mutex<HashSet<Vec<u8>>>,
+        tx_sns: &Mutex<HashSet<Vec<u8>>>,
+        tx_cms: &Mutex<HashSet<Vec<u8>>>,
+        tx_digests: &Mutex<HashSet<Vec<u8>>>,
+        database_fix: &Mutex<Option<DatabaseTransaction>>,
         fix_mode: FixMode,
-        is_storage_valid: &mut bool,
+        is_storage_valid: &AtomicBool,
     ) {
-        for (block_tx_idx, tx) in block.transactions.iter().enumerate() {
+        block.transactions.as_slice().par_iter().enumerate().for_each(|(block_tx_idx, tx)| {
             let tx_id = match tx.transaction_id() {
                 Ok(hash) => hash,
                 Err(e) => {
@@ -330,9 +340,9 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
                         block.header.get_hash(),
                         e
                     );
-                    *is_storage_valid = false;
+                    is_storage_valid.store(false, Ordering::SeqCst);
 
-                    continue;
+                    return;
                 }
             };
 
@@ -341,9 +351,9 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
                     Ok(tx) => tx,
                     Err(e) => {
                         error!("Transaction {} can't be parsed: {}", hex::encode(tx_id), e);
-                        *is_storage_valid = false;
+                        is_storage_valid.store(false, Ordering::SeqCst);
 
-                        continue;
+                        return;
                     }
                 },
                 Err(e) => {
@@ -352,9 +362,9 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
                         hex::encode(tx_id),
                         e
                     );
-                    *is_storage_valid = false;
+                    is_storage_valid.store(false, Ordering::SeqCst);
 
-                    continue;
+                    return;
                 }
             };
 
@@ -364,9 +374,9 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
                         "Transaction {} doesn't have an old serial number stored",
                         hex::encode(tx_id)
                     );
-                    *is_storage_valid = false;
+                    is_storage_valid.store(false, Ordering::SeqCst);
                 }
-                tx_sns.insert(to_bytes!(sn).unwrap()); // to_bytes can't fail
+                tx_sns.lock().insert(to_bytes!(sn).unwrap()); // to_bytes can't fail
             }
 
             for cm in tx.new_commitments() {
@@ -375,9 +385,9 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
                         "Transaction {} doesn't have a new commitment stored",
                         hex::encode(tx_id)
                     );
-                    *is_storage_valid = false;
+                    is_storage_valid.store(false, Ordering::SeqCst);
                 }
-                tx_cms.insert(to_bytes!(cm).unwrap()); // to_bytes can't fail
+                tx_cms.lock().insert(to_bytes!(cm).unwrap()); // to_bytes can't fail
             }
 
             let tx_digest = tx.ledger_digest();
@@ -388,7 +398,7 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
                     hex::encode(tx_id),
                 );
 
-                if let Some(ref mut db_ops) = database_fix {
+                if let Some(ref mut db_ops) = &mut *database_fix.lock() {
                     if [FixMode::MissingTxComponents, FixMode::Everything].contains(&fix_mode) {
                         db_ops.push(Op::Insert {
                             col: COL_DIGEST,
@@ -396,20 +406,20 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
                             value: block_height.to_le_bytes().to_vec(),
                         });
                     } else {
-                        *is_storage_valid = false;
+                        is_storage_valid.store(false, Ordering::SeqCst);
                     }
                 } else {
-                    *is_storage_valid = false;
+                    is_storage_valid.store(false, Ordering::SeqCst);
                 }
             }
-            tx_digests.insert(to_bytes!(tx_digest).unwrap()); // to_bytes can't fail
+            tx_digests.lock().insert(to_bytes!(tx_digest).unwrap()); // to_bytes can't fail
 
             let tx_memo = tx.memorandum();
             if !self.contains_memo(tx_memo) {
                 error!("Transaction {} doesn't have its memo stored", hex::encode(tx_id));
-                *is_storage_valid = false;
+                is_storage_valid.store(false, Ordering::SeqCst);
             }
-            tx_memos.insert(to_bytes!(tx_memo).unwrap()); // to_bytes can't fail
+            tx_memos.lock().insert(to_bytes!(tx_memo).unwrap()); // to_bytes can't fail
 
             match self.get_transaction_location(&tx_id) {
                 Ok(Some(tx_location)) => match self.get_block_number(&BlockHeaderHash(tx_location.block_hash)) {
@@ -421,7 +431,7 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
                                 block_number,
                                 block_height,
                             );
-                            *is_storage_valid = false;
+                            is_storage_valid.store(false, Ordering::SeqCst);
                         }
                     }
                     Err(_) => {
@@ -431,7 +441,7 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
                             BlockHeaderHash(tx_location.block_hash)
                         );
 
-                        if let Some(ref mut db_ops) = database_fix {
+                        if let Some(ref mut db_ops) = &mut *database_fix.lock() {
                             if [FixMode::TxLocations, FixMode::Everything].contains(&fix_mode) {
                                 let corrected_location = TransactionLocation {
                                     index: block_tx_idx as u32,
@@ -448,26 +458,26 @@ impl<T: TransactionScheme, P: LoadableMerkleParameters, S: Storage> Ledger<T, P,
                                     }
                                     Err(e) => {
                                         error!("Can't create a block locator fix for tx {}: {}", hex::encode(tx_id), e);
-                                        *is_storage_valid = false;
+                                        is_storage_valid.store(false, Ordering::SeqCst);
                                     }
                                 }
                             } else {
-                                *is_storage_valid = false;
+                                is_storage_valid.store(false, Ordering::SeqCst);
                             }
                         } else {
-                            *is_storage_valid = false;
+                            is_storage_valid.store(false, Ordering::SeqCst);
                         }
                     }
                 },
                 Err(e) => {
                     error!("Can't get the location of tx {}: {}", hex::encode(tx_id), e);
-                    *is_storage_valid = false;
+                    is_storage_valid.store(false, Ordering::SeqCst);
                 }
                 Ok(None) => {
                     error!("Can't get the location of tx {}", hex::encode(tx_id));
-                    *is_storage_valid = false;
+                    is_storage_valid.store(false, Ordering::SeqCst);
                 }
             }
-        }
+        });
     }
 }
