04be8dd9bec516b838e0bfa808fb5738b9f8f627
p <max.bruce12@gmail.com>
2021-08-25 09:00:59 -0700

    fix build

--- storage/src/storage/storage.rs
+++ storage/src/storage/storage.rs
@@ -47,7 +47,7 @@ pub struct ForkPath {
     pub base_index: u32,
     /// Set of digests from `base_index`'s corresponding block to the target block
     pub path: Vec<Digest>,
-    /// Tree of all descendent blocks to the target block
+    // / Tree of all descendent blocks to the target block
     // pub tail: DigestTree,
 }
 
