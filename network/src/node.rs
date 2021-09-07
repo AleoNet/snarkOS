cf2530867b19ebb6b5be0aec1ce2e74d29c035c5
Protryon <max.bruce12@gmail.com>
2021-09-07 07:47:58 -0700

    fmt

--- network/src/node.rs
+++ network/src/node.rs
@@ -342,10 +342,7 @@ impl Node {
 
         // The node can already be at some non-zero height.
         if self.sync().is_some() {
-            metrics::gauge!(
-                misc::BLOCK_HEIGHT,
-                self.storage.canon().await?.block_height as f64
-            );
+            metrics::gauge!(misc::BLOCK_HEIGHT, self.storage.canon().await?.block_height as f64);
         }
 
         Ok(())
