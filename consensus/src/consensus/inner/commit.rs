f3e6dca7b891ea7e46570ae4a031a0d125cff322
Max Bruce <Protryon@users.noreply.github.com>
2021-10-05 04:19:00 -0700

    Update consensus/src/consensus/inner/commit.rs
    Co-authored-by: ljedrz <ljedrz@users.noreply.github.com>

--- consensus/src/consensus/inner/commit.rs
+++ consensus/src/consensus/inner/commit.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkOS library. If not, see <https://www.gnu.org/licenses/>.
 
-use std::time::Instant;
 
 use super::*;
 use crate::ledger::dummy::DummyLedger;
