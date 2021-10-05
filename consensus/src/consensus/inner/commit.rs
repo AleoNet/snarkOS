7b50ef50da2fe51d220a451e2929d5d4516cd1b6
Protryon <max.bruce12@gmail.com>
2021-10-05 04:26:53 -0700

    fmt

--- consensus/src/consensus/inner/commit.rs
+++ consensus/src/consensus/inner/commit.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkOS library. If not, see <https://www.gnu.org/licenses/>.
 
-
 use super::*;
 use crate::ledger::dummy::DummyLedger;
 use snarkos_metrics as metrics;
