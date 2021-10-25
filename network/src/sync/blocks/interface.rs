10aee38aa3a742dd6fa7564a35b64f4bcf1f5e79
ljedrz <ljedrz@gmail.com>
2021-10-25 14:28:20 +0200

    fix: calculate the internal getblocks rtt even if there's no reply
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- network/src/sync/blocks/interface.rs
+++ network/src/sync/blocks/interface.rs
@@ -177,8 +177,17 @@ impl Node {
             let block = self.storage.get_block(hash).await?;
             blocks.push(block);
         }
-        let blocks: Vec<_> =
-            task::spawn_blocking(move || blocks.into_iter().map(|block| block.serialize()).collect()).await?;
+
+        let blocks: Vec<_> = if !blocks.is_empty() {
+            task::spawn_blocking(move || blocks.into_iter().map(|block| block.serialize()).collect()).await?
+        } else {
+            // Update the related internal RTT metric even if there is no response.
+            if let Some(time_received) = time_received {
+                metrics::histogram!(snarkos_metrics::internal_rtt::GETBLOCKS, time_received.elapsed());
+            }
+
+            return Ok(());
+        };
 
         for (i, (block, hash)) in blocks.into_iter().zip(header_hashes.into_iter()).enumerate() {
             let height = match self.storage.get_block_state(&hash).await? {
