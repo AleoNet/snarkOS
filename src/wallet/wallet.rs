cb736496df2673f048d6cad4b4da67ae5a6c6003
Jules de Smit <julesdesmit@gmail.com>
2021-11-11 13:36:38 -0600

    Migrate text file to RocksDB
    This forgoes the need for synchronizing access to
    the text file and generally simplifies some of the code

--- src/wallet/wallet.rs
+++ src/wallet/wallet.rs
@@ -15,77 +15,58 @@
 // along with the snarkOS library. If not, see <https://www.gnu.org/licenses/>.
 
 use snarkvm::dpc::prelude::*;
-use snarkvm::prelude::{FromBytes, ToBytes};
 
 use anyhow::Result;
-use std::{
-    fs::{self, OpenOptions},
-    io::{BufRead, BufReader, Write},
-    marker::PhantomData,
-    path::{Path, PathBuf},
-};
+use rand::Rng;
+use snarkos_ledger::storage::{rocksdb::RocksDB, DataMap, Map, Storage};
+use std::path::PathBuf;
 
 /// A wallet, belonging to a specific address. Currently keeps records of all
 /// coinbases ever mined to this wallet, regardless of whether they are valid
 /// or not on the current chain.
 ///
 /// The path leading to the file containing all records will be structured as
-/// {data_path}/{address}/data.txt.
+/// {data_path}/{address}/.
+///
+/// The database will just store coinbase transactions under random IDs, as we
+/// don't really currently need to index them with anything, and the only operations
+/// we perform are storing single records, or fetching the entire list.
 pub struct Wallet<N: Network> {
     /// The address associated with this wallet.
     address: String,
     /// A path to a directory containing all coinbase records.
     data_path: String,
-    _phantom: PhantomData<N>,
+    /// The RocksDB containing all records for the given address.
+    db: DataMap<u64, Transaction<N>>,
 }
 
 impl<N: Network> Wallet<N> {
     /// Creates a new [`Wallet`], and initializes a text file in the resulting
     /// path, if it doesn't yet exist.
     pub fn new(address: String, data_path: String) -> Result<Self> {
-        let wallet = Self {
+        let path = PathBuf::from(format!("{}/{}/", data_path, address));
+
+        Ok(Self {
             address,
             data_path,
-            _phantom: PhantomData,
-        };
-        if !wallet.path().exists() {
-            fs::File::create(wallet.path())?;
-        }
-
-        Ok(wallet)
-    }
-
-    pub fn path_as_string(&self) -> String {
-        format!("{}/{}/data.txt", self.data_path, self.address)
-    }
-
-    pub fn path(&self) -> PathBuf {
-        Path::new(&self.path_as_string()).into()
+            db: RocksDB::open(path, 0, false)?.open_map(&"wallet")?,
+        })
     }
 
     /// Fetch all records from the database.
     pub fn records(&self) -> Result<Vec<Transaction<N>>> {
-        let f = fs::File::open(self.path())?;
-        let lines = BufReader::new(f).lines();
         let mut records = vec![];
 
-        for (i, line) in lines.enumerate() {
-            if let Ok(transaction_data) = line {
-                let record = Transaction::<N>::read_le(transaction_data.as_bytes())?;
-                records.push(record);
-            } else {
-                error!("Wallet file contains malformed transaction record at line {}", i);
-            }
+        for record in self.db.values() {
+            records.push(record);
         }
 
         Ok(records)
     }
 
     /// Push a record to the database.
-    pub fn push_record(&self, t: Transaction<N>) -> Result<()> {
-        let mut f = OpenOptions::new().write(true).open(self.path())?;
-        t.write_le(&mut f)?;
-        f.write(b"\n")?;
-        Ok(())
+    pub fn push_record(&self, t: &Transaction<N>) -> Result<()> {
+        let id = rand::thread_rng().gen::<u64>();
+        self.db.insert(&id, t)
     }
 }
