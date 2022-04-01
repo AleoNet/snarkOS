7b977e3719b76c1875a05e4d1c867311bb24ac30
ljedrz <ljedrz@gmail.com>
2022-04-01 15:58:23 +0200

    tests: add another fuzz test
    Signed-off-by: ljedrz <ljedrz@gmail.com>

---
+++ fuzz/fuzz_targets/fuzz_target_2.rs
@@ -0,0 +1,36 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+use snarkos_environment::{network::Message, Client, CurrentNetwork};
+
+fuzz_target!(|message: &[u8]| {
+    if let Ok(message) = Message::<CurrentNetwork, Client<CurrentNetwork>>::deserialize(message.into()) {
+        match message {
+            Message::BlockResponse(data) => {
+                let _ = data.deserialize_blocking();
+            }
+            Message::ChallengeResponse(data) => {
+                let _ = data.deserialize_blocking();
+            }
+            Message::Ping(.., data) => {
+                let _ = data.deserialize_blocking();
+            }
+            Message::Pong(.., data) => {
+                let _ = data.deserialize_blocking();
+            }
+            Message::UnconfirmedBlock(.., data) => {
+                let _ = data.deserialize_blocking();
+            }
+            Message::UnconfirmedTransaction(data) => {
+                let _ = data.deserialize_blocking();
+            }
+            Message::PoolRequest(.., data) => {
+                let _ = data.deserialize_blocking();
+            }
+            Message::PoolResponse(.., data) => {
+                let _ = data.deserialize_blocking();
+            }
+            _ => {}
+        }
+    }
+});
