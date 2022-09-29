d5fda5a79e87e338a79a963dd804f57829b3406c
raychu86 <14917648+raychu86@users.noreply.github.com>
2022-09-29 10:08:33 -0700

    Remove development log

--- snarkos/ledger/mod.rs
+++ snarkos/ledger/mod.rs
@@ -385,16 +385,12 @@ impl<N: Network, E: Environment> Ledger<N, E> {
                 // If the peer is ahead, ask for next block.
                 let latest_height = self.ledger().read().latest_height();
                 if height > latest_height {
-                    println!("SENDING A FUCK TON OF THE SAME REQUEST:");
-
-                    for _ in 0..200 {
-                        if let Err(err) = self
-                            .peers_router()
-                            .send(PeersRequest::MessageSend(peer_ip, Message::<N>::BlockRequest(latest_height + 1)))
-                            .await
-                        {
-                            warn!("[BlockRequest] {}", err);
-                        }
+                    if let Err(err) = self
+                        .peers_router()
+                        .send(PeersRequest::MessageSend(peer_ip, Message::<N>::BlockRequest(latest_height + 1)))
+                        .await
+                    {
+                        warn!("[BlockRequest] {}", err);
                     }
                 }
             }
