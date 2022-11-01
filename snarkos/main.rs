4bd56cf710a6761d2104d576ff7562239d3fe06d
ljedrz <ljedrz@gmail.com>
2022-11-01 22:00:48 +0100

    feat: use jemalloc as the global allocator
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- snarkos/main.rs
+++ snarkos/main.rs
@@ -18,6 +18,11 @@ use snarkos_cli::{commands::CLI, helpers::Updater};
 
 use clap::Parser;
 
+use tikv_jemallocator::Jemalloc;
+
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 fn main() -> anyhow::Result<()> {
     // Parse the given arguments.
     let cli = CLI::parse();
