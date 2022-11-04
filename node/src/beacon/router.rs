1169a409c4a021c7ff7bddf6915e95be814c62b5
raychu86 <14917648+raychu86@users.noreply.github.com>
2022-11-04 16:02:43 -0700

    Beacons only propagate unconfirmed solutions to other beacons

--- node/src/beacon/router.rs
+++ node/src/beacon/router.rs
@@ -81,8 +81,8 @@ impl<N: Network> Inbound<N> for Beacon<N> {
                 return true; // Maintain the connection.
             }
 
-            // Propagate the `UnconfirmedSolution`.
-            let request = RouterRequest::MessagePropagate(Message::UnconfirmedSolution(message), vec![peer_ip]);
+            // Propagate the `UnconfirmedSolution` to connected beacons.
+            let request = RouterRequest::MessagePropagateBeacon(Message::UnconfirmedSolution(message), vec![peer_ip]);
             if let Err(error) = router.process(request).await {
                 warn!("[UnconfirmedSolution] {error}");
             }
