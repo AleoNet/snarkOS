ee29cf2e0040a76c787e42d44c0243d6dcab9499
raychu86 <14917648+raychu86@users.noreply.github.com>
2022-11-05 00:08:58 -0700

    Implement JWT

--- node/rest/src/helpers/mod.rs
+++ node/rest/src/helpers/mod.rs
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkOS library. If not, see <https://www.gnu.org/licenses/>.
 
+mod auth;
+pub use auth::*;
+
 mod error;
 pub use error::*;
 
