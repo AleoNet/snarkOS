33f67916a31e13edf4e28667c56ecf8af45ffae2
Howard Wu <9260812+howardwu@users.noreply.github.com>
2022-11-10 18:04:55 -0800

    Recompute challenge on new epoch

--- node/ledger/src/lib.rs
+++ node/ledger/src/lib.rs
@@ -233,18 +233,18 @@ impl<N: Network, C: ConsensusStorage<N>> Ledger<N, C> {
     pub fn add_next_block(&self, block: &Block<N>) -> Result<()> {
         // Acquire the write lock on the current block.
         let mut current_block = self.current_block.write();
-        // Acquire the write lock on the current epoch challenge.
-        let mut current_epoch_challenge = self.current_epoch_challenge.write();
-
         // Update the VM.
         self.vm.add_next_block(block)?;
-        // Update the epoch challenge.
-        let epoch_challenge = self.get_epoch_challenge(block.height()).ok();
-
         // Update the current block.
         *current_block = block.clone();
-        // Update the current epoch challenge.
-        current_epoch_challenge.clone_from(&epoch_challenge);
+        // Drop the write lock on the current block.
+        drop(current_block);
+
+        // If the block is the start of a new epoch, or the epoch challenge has not been set, update the current epoch challenge.
+        if block.height() % N::NUM_BLOCKS_PER_EPOCH == 0 || self.current_epoch_challenge.read().is_none() {
+            // Update the current epoch challenge.
+            self.current_epoch_challenge.write().clone_from(&self.get_epoch_challenge(block.height()).ok());
+        }
 
         Ok(())
     }
