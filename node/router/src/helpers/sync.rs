489789b8aabe0e2b7d43c430f95b24c56c271747
Howard Wu <9260812+howardwu@users.noreply.github.com>
2022-11-22 19:21:03 -0800

    Simplify sync module, further abstraction

--- node/router/src/helpers/sync.rs
+++ node/router/src/helpers/sync.rs
@@ -17,16 +17,74 @@
 use snarkvm::prelude::Network;
 
 use anyhow::{bail, Result};
-use indexmap::{map::Entry, IndexMap};
+use indexmap::IndexMap;
+use itertools::Itertools;
 use parking_lot::RwLock;
 use std::{net::SocketAddr, sync::Arc};
 
+#[derive(Clone, Debug)]
+pub struct Sync<N: Network> {
+    /// The map of peer IPs to their block locators.
+    locators: Arc<RwLock<IndexMap<SocketAddr, BlockLocators<N>>>>,
+}
+
+impl<N: Network> Default for Sync<N> {
+    /// Initializes a new instance of the sync module.
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<N: Network> Sync<N> {
+    /// Initializes a new instance of the sync module.
+    pub fn new() -> Self {
+        Self { locators: Default::default() }
+    }
+
+    /// Returns the list of peers with their heights, sorted by height (descending).
+    pub fn get_peers_by_height(&self) -> Vec<(SocketAddr, u32)> {
+        self.locators
+            .read()
+            .iter()
+            .map(|(peer_ip, locators)| (*peer_ip, locators.latest_height()))
+            .sorted_by(|(_, a), (_, b)| b.cmp(a))
+            .collect()
+    }
+
+    /// Updates the block locators for the given peer IP.
+    /// This function ensures all peers share a consistent view of the ledger.
+    pub fn update_peer(&self, peer_ip: SocketAddr, locators: BlockLocators<N>) -> Result<()> {
+        // Ensure the given block locators are well-formed.
+        locators.ensure_is_valid()?;
+
+        // Acquire the write lock on the locators map.
+        let mut locators_write = self.locators.write();
+
+        // Ensure the locators are consistent with the block locators of every peer (including itself).
+        for (_, peer_locators) in locators_write.iter() {
+            locators.ensure_is_consistent_with(peer_locators)?;
+        }
+
+        // Update the locators entry for the given peer IP.
+        locators_write.entry(peer_ip).or_insert(locators);
+        Ok(())
+    }
+
+    /// Removes the peer, if they exist.
+    pub fn remove_peer(&self, peer_ip: SocketAddr) {
+        // Remove the locators entry for the given peer IP.
+        self.locators.write().remove(&peer_ip);
+    }
+}
+
+/// TODO (howardwu): Move everything below here to a separate crate (maybe to ledger or messages).
+
 /// The number of recent blocks (near tip).
-const NUM_RECENTS: usize = 100; // 100 blocks
+pub const NUM_RECENTS: usize = 100; // 100 blocks
 /// The interval between recent blocks.
-const RECENT_INTERVAL: u32 = 1; // 1 block intervals
+pub const RECENT_INTERVAL: u32 = 1; // 1 block intervals
 /// The interval between block checkpoints.
-const CHECKPOINT_INTERVAL: u32 = 10_000; // 10,000 block intervals
+pub const CHECKPOINT_INTERVAL: u32 = 10_000; // 10,000 block intervals
 
 #[derive(Clone, Debug)]
 pub struct BlockLocators<N: Network> {
@@ -36,71 +94,58 @@ pub struct BlockLocators<N: Network> {
     pub checkpoints: IndexMap<u32, N::BlockHash>,
 }
 
-/// The assumed structure (in order of block height) is: `\[ checkpoints || recents || height \]`
-#[derive(Clone, Debug)]
-pub struct Sync<N: Network> {
-    /// The map of peer IPs to their last known block height.
-    heights: Arc<RwLock<IndexMap<SocketAddr, u32>>>,
-    /// The map of peer IPs to their block locators.
-    locators: Arc<RwLock<IndexMap<SocketAddr, BlockLocators<N>>>>,
-}
-
-impl<N: Network> Default for Sync<N> {
-    /// Initializes a new instance of the sync manager.
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl<N: Network> Sync<N> {
-    /// Initializes a new instance of the sync manager.
-    pub fn new() -> Self {
-        Self { heights: Default::default(), locators: Default::default() }
+impl<N: Network> BlockLocators<N> {
+    /// Initializes a new instance of the block locators.
+    pub fn new(recents: IndexMap<u32, N::BlockHash>, checkpoints: IndexMap<u32, N::BlockHash>) -> Self {
+        Self { recents, checkpoints }
     }
 
-    /// Updates the last known block height and block checkpoints for the given peer IP.
-    /// This function ensures the given recent blocks and checkpoints are consistent with the previous ones.
-    pub fn update_peer(&self, peer_ip: SocketAddr, height: u32, locators: BlockLocators<N>) -> Result<()> {
+    /// Returns the latest height.
+    pub fn latest_height(&self) -> u32 {
+        self.recents.keys().last().copied().unwrap_or_default()
+    }
+
+    /// Returns `true` if the block locators are well-formed.
+    pub fn is_valid(&self) -> bool {
         // Ensure the block locators are well-formed.
-        Self::check_block_locators(height, &locators.recents, &locators.checkpoints)?;
-
-        // Acquire the write lock on the heights map.
-        let mut heights_write = self.heights.write();
-        // Acquire the write lock on the locators map.
-        let mut locators_write = self.locators.write();
-
-        // Retrieve the height entry for the given peer IP.
-        let height_entry = heights_write.entry(peer_ip).or_default();
-        // Retrieve the locators entry for the given peer IP.
-        let locators_entry = locators_write.entry(peer_ip);
-
-        // Update the locators entry for the given peer IP.
-        match locators_entry {
-            // If the locators entry already exists, ensure the new locators are consistent with the old ones, and insert.
-            Entry::Occupied(mut entry) => {
-                let old_locators = entry.get();
-                // Ensure the block locators are consistent with the previous ones.
-                Self::check_consistent_block_locators(old_locators, &locators)?;
-                // Insert the new block locators.
-                entry.insert(locators);
-            }
-            // Otherwise, insert the new locators.
-            Entry::Vacant(entry) => {
-                entry.insert(locators);
-            }
+        if let Err(error) = self.ensure_is_valid() {
+            warn!("Block locators are invalid: {error}");
+            return false;
         }
+        true
+    }
 
-        // Lastly, update the height entry for the given peer IP.
-        *height_entry = height;
+    /// Returns `true` if the given block locators are consistent with this one.
+    /// This function assumes the given block locators are well-formed.
+    pub fn is_consistent_with(&self, other: &Self) -> bool {
+        // Ensure the block locators are consistent with the previous ones.
+        if let Err(error) = self.ensure_is_consistent_with(other) {
+            warn!("Inconsistent block locators: {error}");
+            return false;
+        }
+        true
+    }
 
-        Ok(())
+    /// Checks that this block locators are well-formed.
+    pub fn ensure_is_valid(&self) -> Result<()> {
+        // Ensure the block locators are well-formed.
+        Self::check_block_locators(&self.recents, &self.checkpoints)
+    }
+
+    /// Returns `true` if the given block locators are consistent with this one.
+    /// This function assumes the given block locators are well-formed.
+    pub fn ensure_is_consistent_with(&self, other: &Self) -> Result<()> {
+        Self::check_consistent_block_locators(self, other)
     }
 }
 
-impl<N: Network> Sync<N> {
+impl<N: Network> BlockLocators<N> {
     /// Checks the old and new block locators share a consistent view of block history.
     /// This function assumes the given block locators are well-formed.
-    fn check_consistent_block_locators(old_locators: &BlockLocators<N>, new_locators: &BlockLocators<N>) -> Result<()> {
+    pub fn check_consistent_block_locators(
+        old_locators: &BlockLocators<N>,
+        new_locators: &BlockLocators<N>,
+    ) -> Result<()> {
         // For the overlapping recent blocks, ensure their block hashes match.
         for (height, hash) in new_locators.recents.iter() {
             if let Some(recent_hash) = old_locators.recents.get(height) {
@@ -121,15 +166,14 @@ impl<N: Network> Sync<N> {
     }
 
     /// Checks that the block locators are well-formed.
-    fn check_block_locators(
-        height: u32,
+    pub fn check_block_locators(
         recents: &IndexMap<u32, <N as Network>::BlockHash>,
         checkpoints: &IndexMap<u32, <N as Network>::BlockHash>,
     ) -> Result<()> {
         // Ensure the recent blocks are well-formed.
-        let last_recent_height = Self::check_recent_blocks(height, recents)?;
+        let last_recent_height = Self::check_recent_blocks(recents)?;
         // Ensure the block checkpoints are well-formed.
-        let last_checkpoint_height = Self::check_block_checkpoints(height, checkpoints)?;
+        let last_checkpoint_height = Self::check_block_checkpoints(checkpoints)?;
         // Ensure the last recent height is at or above the last checkpoint height.
         if last_recent_height < last_checkpoint_height {
             bail!("Recent height ({last_recent_height}) cannot be below checkpoint ({last_checkpoint_height})")
@@ -145,8 +189,7 @@ impl<N: Network> Sync<N> {
     /// 3. The map is at the correct height.
     /// 4. The map is in the correct order.
     /// 5. The map does not contain too many entries.
-    /// 6. The given height is the last recent block.
-    fn check_recent_blocks(height: u32, recents: &IndexMap<u32, N::BlockHash>) -> Result<u32> {
+    fn check_recent_blocks(recents: &IndexMap<u32, N::BlockHash>) -> Result<u32> {
         // Ensure the number of recent blocks is at least 1.
         if recents.is_empty() {
             bail!("There must be at least 1 recent block")
@@ -169,11 +212,6 @@ impl<N: Network> Sync<N> {
             last_height = *current_height;
         }
 
-        // Ensure the given height is the last recent height.
-        if height != last_height {
-            bail!("Block height must be the last recent height")
-        }
-
         // If the last height is below NUM_RECENTS, ensure the number of recent blocks matches the last height.
         if last_height < NUM_RECENTS as u32 && recents.len() as u32 != last_height {
             bail!("As the last height is below {NUM_RECENTS}, the number of recent blocks must match the height")
@@ -193,8 +231,7 @@ impl<N: Network> Sync<N> {
     /// 2. The block checkpoints are at the correct interval.
     /// 3. The block checkpoints are at the correct height.
     /// 4. The block checkpoints are in the correct order.
-    /// 5. The given height is at or above the last checkpoint height.
-    fn check_block_checkpoints(height: u32, checkpoints: &IndexMap<u32, <N as Network>::BlockHash>) -> Result<u32> {
+    fn check_block_checkpoints(checkpoints: &IndexMap<u32, <N as Network>::BlockHash>) -> Result<u32> {
         // Ensure the block checkpoints are not empty.
         assert!(!checkpoints.is_empty());
 
@@ -213,11 +250,9 @@ impl<N: Network> Sync<N> {
             last_height = *current_height;
         }
 
-        // Ensure the given height is at or above the last checkpoint height.
-        if height < last_height {
-            bail!("Block height must be at or above the last checkpoint height")
-        }
-
         Ok(last_height)
     }
 }
+
+#[cfg(test)]
+mod tests {}
