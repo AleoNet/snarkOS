a521378413a7325c2a5c0d4192b017a11a6eb06b
ghostant1017 <bolin@kunyaokeji.com>
2022-12-06 14:59:00 +0800

    fix checkpoints initialize

--- node/src/helpers.rs
+++ node/src/helpers.rs
@@ -35,7 +35,7 @@ pub fn get_block_locators<N: Network, C: ConsensusStorage<N>>(ledger: &Ledger<N,
     }
 
     // Initialize the checkpoints map.
-    let mut checkpoints = IndexMap::with_capacity((latest_height % CHECKPOINT_INTERVAL).try_into()?);
+    let mut checkpoints = IndexMap::with_capacity((latest_height / CHECKPOINT_INTERVAL).try_into()?);
 
     // Retrieve the checkpoint block hashes.
     for height in (0..=latest_height).step_by(CHECKPOINT_INTERVAL as usize) {
