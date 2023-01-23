0911547891ef7d6a4e67652b1cd6b19b99c1d553
ljedrz <ljedrz@gmail.com>
2023-01-23 17:18:52 +0100

    feat: make the handshake asymmetric
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- node/router/src/helpers/peer.rs
+++ node/router/src/helpers/peer.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with the snarkOS library. If not, see <https://www.gnu.org/licenses/>.
 
-use snarkos_node_messages::NodeType;
+use snarkos_node_messages::{ChallengeRequest, NodeType};
 use snarkvm::prelude::{Address, Network};
 
 use parking_lot::RwLock;
@@ -39,12 +39,12 @@ pub struct Peer<N: Network> {
 
 impl<N: Network> Peer<N> {
     /// Initializes a new instance of `Peer`.
-    pub fn new(listening_ip: SocketAddr, address: Address<N>, node_type: NodeType, version: u32) -> Self {
+    pub fn new(listening_ip: SocketAddr, challenge_request: &ChallengeRequest<N>) -> Self {
         Self {
             peer_ip: listening_ip,
-            address,
-            node_type,
-            version,
+            address: challenge_request.address,
+            node_type: challenge_request.node_type,
+            version: challenge_request.version,
             first_seen: Instant::now(),
             last_seen: Arc::new(RwLock::new(Instant::now())),
         }
