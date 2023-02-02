a78c020d20a106c6a634f1a850512c651c826ca8
Niklas <niklas@long.ch>
2023-02-02 14:50:03 +0100

    feat: implement bare-bones metrics lib

---
+++ metrics/src/names.rs
@@ -0,0 +1,27 @@
+// Copyright (C) 2019-2022 Aleo Systems Inc.
+// This file is part of the snarkOS library.
+
+// The snarkOS library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The snarkOS library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with the snarkOS library. If not, see <https://www.gnu.org/licenses/>.
+
+pub const GAUGE_NAMES: [&str; 4] = [blocks::HEIGHT, peers::CONNECTED, peers::CANDIDATE, peers::RESTRICTED];
+
+pub mod blocks {
+    pub const HEIGHT: &str = "snarkos_blocks_height_total";
+}
+
+pub mod peers {
+    pub const CONNECTED: &str = "snarkos_peers_connected_total";
+    pub const CANDIDATE: &str = "snarkos_peers_candidate_total";
+    pub const RESTRICTED: &str = "snarkos_peers_restricted_total";
+}
