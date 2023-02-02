b39cdcb1b081adeee16967c60beaf37ba94aa2c8
Niklas <niklas@long.ch>
2023-02-02 14:50:03 +0100

    feat: implement bare-bones metrics lib

--- metrics/src/lib.rs
+++ metrics/src/lib.rs
@@ -13,3 +13,36 @@
 
 // You should have received a copy of the GNU General Public License
 // along with the snarkOS library. If not, see <https://www.gnu.org/licenses/>.
+
+mod names;
+
+// Re-export the metrics macros.
+pub use metrics::*;
+// Expose the names at the crate level for easy access.
+pub use names::*;
+
+/// Initialises the metrics and returns a handle to the task running the metrics exporter.
+pub fn initialize() -> tokio::task::JoinHandle<()> {
+    use metrics_exporter_prometheus::PrometheusBuilder;
+
+    // Build the recorder and set as global.
+    let (recorder, exporter) = PrometheusBuilder::new().build().expect("can't build the prometheus exporter");
+    metrics::set_boxed_recorder(Box::new(recorder)).expect("can't set the prometheus exporter");
+
+    // Spawn a dedicated task for the exporter on the runtime.
+    let metrics_exporter_task = tokio::task::spawn(async move {
+        exporter.await.expect("can't await the prometheus exporter");
+    });
+
+    // Register the metrics so they exist on init.
+    register_metrics();
+
+    // Return the exporter's task handle to be tracked by the node's task handling.
+    metrics_exporter_task
+}
+
+fn register_metrics() {
+    for name in GAUGE_NAMES {
+        register_gauge!(name);
+    }
+}
