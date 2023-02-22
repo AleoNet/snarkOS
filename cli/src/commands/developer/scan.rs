c32077a12aafe50d10f659fce227371a39b3349e
raychu86 <14917648+raychu86@users.noreply.github.com>
2023-02-21 17:53:18 -0800

    Update scan

--- cli/src/commands/developer/scan.rs
+++ cli/src/commands/developer/scan.rs
@@ -16,9 +16,9 @@
 
 use super::CurrentNetwork;
 
-use snarkvm::prelude::{Block, Plaintext, Record, ViewKey};
+use snarkvm::prelude::{Block, Ciphertext, Field, Network, Plaintext, PrivateKey, Record, ViewKey};
 
-use anyhow::{bail, Result};
+use anyhow::{bail, ensure, Result};
 use clap::Parser;
 use std::str::FromStr;
 
@@ -26,17 +26,21 @@ use std::str::FromStr;
 /// Scan the snarkOS node for records.
 #[derive(Debug, Parser)]
 pub struct Scan {
-    /// The view key used to scan the snarkOS node.
+    /// An optional private key scan for unspent records.
     #[clap(short, long)]
-    pub view_key: String,
+    private_key: Option<String>,
+
+    /// The view key used to scan for records.
+    #[clap(short, long)]
+    view_key: Option<String>,
 
     /// The block height to start scanning from.
     #[clap(long, default_value = "0")]
-    pub start: u32,
+    start: u32,
 
     /// The block height to stop scanning.
     #[clap(long)]
-    pub end: Option<u32>,
+    end: Option<u32>,
 
     /// The endpoint to scan blocks from.
     #[clap(long)]
@@ -45,9 +49,60 @@ pub struct Scan {
 
 impl Scan {
     pub fn parse(self) -> Result<String> {
-        // Derive the view key.
-        let view_key = ViewKey::<CurrentNetwork>::from_str(&self.view_key)?;
+        // Derive the view key and optional private key.
+        let (private_key, view_key) = self.parse_account()?;
 
+        // Find the end height.
+        let end = self.parse_end_height()?;
+
+        // Fetch the records from the network.
+        let records = Self::fetch_records(private_key, &view_key, &self.endpoint, self.start, end)?;
+
+        // Output the decrypted records associated with the view key.
+        if records.is_empty() {
+            Ok("No records found".to_string())
+        } else {
+            if private_key.is_none() {
+                println!("⚠️  This list may contain records that have already been spent.\n");
+            }
+
+            Ok(serde_json::to_string_pretty(&records)?.replace("\\n", ""))
+        }
+    }
+
+    /// Returns the view key and optional private key, from the given configurations.
+    fn parse_account<N: Network>(&self) -> Result<(Option<PrivateKey<N>>, ViewKey<N>)> {
+        match (&self.private_key, &self.view_key) {
+            (Some(private_key), Some(view_key)) => {
+                // Derive the private key.
+                let private_key = PrivateKey::<N>::from_str(&private_key)?;
+                // Derive the expected view key.
+                let expected_view_key = ViewKey::<N>::try_from(&private_key)?;
+                // Derive the view key.
+                let view_key = ViewKey::<N>::from_str(&view_key)?;
+
+                ensure!(
+                    expected_view_key == view_key,
+                    "The provided private key does not correspond to the provided view key."
+                );
+
+                Ok((Some(private_key), view_key))
+            }
+            (Some(private_key), _) => {
+                // Derive the private key.
+                let private_key = PrivateKey::<N>::from_str(&private_key)?;
+                // Derive the view key.
+                let view_key = ViewKey::<N>::try_from(&private_key)?;
+
+                Ok((Some(private_key), view_key))
+            }
+            (None, Some(view_key)) => Ok((None, ViewKey::<N>::from_str(&view_key)?)),
+            (None, None) => bail!("Missing private key or view key."),
+        }
+    }
+
+    /// Returns latest block hash to request.
+    fn parse_end_height(&self) -> Result<u32> {
         // Find the end height.
         let end = match self.end {
             Some(height) => height,
@@ -65,24 +120,16 @@ impl Scan {
             }
         };
 
-        // Fetch the records from the network.
-        let records = Self::fetch_records(&view_key, self.endpoint, self.start, end)?;
+        ensure!(end > self.start, "The given scan range is invalid (start = {}, end = {end})", self.start);
 
-        // Output the decrypted records associated with the view key.
-        if records.is_empty() {
-            Ok("No records found".to_string())
-        } else {
-            println!("⚠️  This list may contain records that have already been spent.\n");
-
-            Ok(serde_json::to_string_pretty(&records)?.replace("\\n", ""))
-        }
+        Ok(end)
     }
 
-    // TODO (raychu86):Account for spent records.
-    /// Fetch owned records from the endpoint.
+    /// Fetch owned ciphertext records from the endpoint.
     pub fn fetch_records(
+        private_key: Option<PrivateKey<CurrentNetwork>>,
         view_key: &ViewKey<CurrentNetwork>,
-        endpoint: String,
+        endpoint: &str,
         start_height: u32,
         end_height: u32,
     ) -> Result<Vec<Record<CurrentNetwork, Plaintext<CurrentNetwork>>>> {
@@ -101,23 +148,29 @@ impl Scan {
         // Scan the endpoint starting from the start height
         let mut request_start = start_height;
         while request_start <= end_height {
+            // TODO (raychu86): Add progress bar.
+
             let num_blocks_to_request =
                 std::cmp::min(MAX_BLOCK_RANGE, end_height.saturating_sub(request_start).saturating_add(1));
             let request_end = request_start.saturating_add(num_blocks_to_request);
 
             // Establish the endpoint.
-            let endpoint = format!("{endpoint}/testnet3/blocks?start={request_start}&end={request_end}");
+            let blocks_endpoint = format!("{endpoint}/testnet3/blocks?start={request_start}&end={request_end}");
 
             // Fetch blocks
-            let blocks: Vec<Block<CurrentNetwork>> = ureq::get(&endpoint).call()?.into_json()?;
+            let blocks: Vec<Block<CurrentNetwork>> = ureq::get(&blocks_endpoint).call()?.into_json()?;
 
             // Scan the blocks for owned records.
             for block in &blocks {
-                for (_, record) in block.records() {
+                for (commitment, ciphertext_record) in block.records() {
                     // Check if the record is owned by the given view key.
-                    if record.is_owner_with_address_x_coordinate(view_key, &address_x_coordinate) {
-                        // Decrypt the record.
-                        records.push(record.decrypt(view_key)?);
+                    if ciphertext_record.is_owner_with_address_x_coordinate(view_key, &address_x_coordinate) {
+                        // Decrypt and optionally filter the records.
+                        if let Some(record) =
+                            Self::decrypt_record(private_key, view_key, endpoint, *commitment, ciphertext_record)?
+                        {
+                            records.push(record);
+                        }
                     }
                 }
             }
@@ -126,5 +179,113 @@ impl Scan {
         }
 
         Ok(records)
+    }
+
+    /// Decrypts the ciphertext record and filters spend record if a private key was provided.
+    fn decrypt_record(
+        private_key: Option<PrivateKey<CurrentNetwork>>,
+        view_key: &ViewKey<CurrentNetwork>,
+        endpoint: &str,
+        commitment: Field<CurrentNetwork>,
+        ciphertext_record: &Record<CurrentNetwork, Ciphertext<CurrentNetwork>>,
+    ) -> Result<Option<Record<CurrentNetwork, Plaintext<CurrentNetwork>>>> {
+        // Check if a private key was provided.
+        if let Some(private_key) = private_key {
+            // Compute the serial number.
+            let serial_number =
+                Record::<CurrentNetwork, Plaintext<CurrentNetwork>>::serial_number(private_key, commitment)?;
+
+            // Establish the endpoint.
+            let endpoint = format!("{endpoint}/testnet3/find/transitionID/{serial_number}");
+
+            // Check if the record is spent.
+            match ureq::get(&endpoint).call() {
+                // On success, skip as the record is spent.
+                Ok(_) => Ok(None),
+                // On error, add the record.
+                Err(_error) => {
+                    // TODO: Dedup the error types. We're adding the record as valid because the endpoint failed,
+                    //  meaning it couldn't find the serial number (ie. unspent). However if there's a DNS error or request error,
+                    //  we have a false positive here then.
+                    // Decrypt the record.
+                    Ok(Some(ciphertext_record.decrypt(view_key)?))
+                }
+            }
+        } else {
+            // If no private key was provided, return the record.
+            Ok(Some(ciphertext_record.decrypt(view_key)?))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use snarkvm::prelude::{TestRng, Testnet3};
+
+    type CurrentNetwork = Testnet3;
+
+    #[test]
+    fn test_parse_account() {
+        let rng = &mut TestRng::default();
+
+        // Generate private key and view key.
+        let private_key = PrivateKey::<CurrentNetwork>::new(rng).unwrap();
+        let view_key = ViewKey::try_from(private_key).unwrap();
+
+        // Generate unassociated private key and view key.
+        let unassociated_private_key = PrivateKey::<CurrentNetwork>::new(rng).unwrap();
+        let unassociated_view_key = ViewKey::try_from(unassociated_private_key).unwrap();
+
+        let config = Scan::try_parse_from(
+            [
+                "snarkos",
+                "--private-key",
+                &format!("{private_key}"),
+                "--view-key",
+                &format!("{view_key}"),
+                "--start",
+                "0",
+                "--end",
+                "10",
+                "--endpoint",
+                "",
+            ]
+            .iter(),
+        )
+        .unwrap();
+        assert!(config.parse_account::<CurrentNetwork>().is_ok());
+
+        let config = Scan::try_parse_from(
+            [
+                "snarkos",
+                "--private-key",
+                &format!("{private_key}"),
+                "--view-key",
+                &format!("{unassociated_view_key}"),
+                "--start",
+                "0",
+                "--end",
+                "10",
+                "--endpoint",
+                "",
+            ]
+            .iter(),
+        )
+        .unwrap();
+        assert!(config.parse_account::<CurrentNetwork>().is_err());
+    }
+
+    #[test]
+    fn test_parse_end_height() {
+        let config =
+            Scan::try_parse_from(["snarkos", "--view-key", "", "--start", "0", "--end", "10", "--endpoint", ""].iter())
+                .unwrap();
+        assert!(config.parse_end_height().is_ok());
+
+        let config =
+            Scan::try_parse_from(["snarkos", "--view-key", "", "--start", "10", "--end", "5", "--endpoint", ""].iter())
+                .unwrap();
+        assert!(config.parse_end_height().is_err());
     }
 }
