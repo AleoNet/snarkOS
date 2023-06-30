cf0062524be099895ffa4a0e02d192460ebca33e
raychu86 <14917648+raychu86@users.noreply.github.com>
2023-06-30 16:07:13 -0700

    Bump message version to 8

--- node/messages/src/lib.rs
+++ node/messages/src/lib.rs
@@ -126,7 +126,7 @@ pub enum Message<N: Network> {
 
 impl<N: Network> Message<N> {
     /// The version of the network protocol; it can be incremented in order to force users to update.
-    pub const VERSION: u32 = 7;
+    pub const VERSION: u32 = 8;
 
     /// Returns the message name.
     #[inline]
