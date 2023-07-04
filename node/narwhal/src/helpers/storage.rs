ac7d6e83dbe3a7ecee4167619ccbbd38b38ebbe0
Howard Wu <9260812+howardwu@users.noreply.github.com>
2023-07-04 09:22:03 -0700

    Improve storage, add remove_cert method, add test

--- node/narwhal/src/helpers/storage.rs
+++ node/narwhal/src/helpers/storage.rs
@@ -22,12 +22,27 @@ use indexmap::{IndexMap, IndexSet};
 use parking_lot::RwLock;
 use std::sync::Arc;
 
+/// The storage for the memory pool.
+///
+/// The storage is used to store the following:
+/// - `round` to `certificate ID` entries.
+/// - `certificate ID` to `certificate` entries.
+/// - `batch ID` to `round` entries.
+/// - `transmission ID` to `transmission` entries.
+///
+/// The chain of events is as follows:
+/// 1. A `transmission` is received.
+/// 2. The `transmission` is added to the `transmissions` map.
+/// 3. After a `batch` is ready to be stored:
+///   - The `certificate` triggers updates to the `rounds`, `certificates`, and `batch_ids` maps.
 #[derive(Clone, Debug)]
 pub struct Storage<N: Network> {
-    /// The map of `round` to a list of `(certificate ID, address)` entries.
-    rounds: Arc<RwLock<IndexMap<u64, IndexSet<(Field<N>, Address<N>)>>>>,
+    /// The map of `round` to a list of `(certificate ID, batch ID, address)` entries.
+    rounds: Arc<RwLock<IndexMap<u64, IndexSet<(Field<N>, Field<N>, Address<N>)>>>>,
     /// The map of `certificate ID` to `certificate`.
     certificates: Arc<RwLock<IndexMap<Field<N>, BatchCertificate<N>>>>,
+    /// The map of `batch ID` to `round`.
+    batch_ids: Arc<RwLock<IndexMap<Field<N>, u64>>>,
     /// The map of `transmission ID` to `transmission`.
     transmissions: Arc<RwLock<IndexMap<TransmissionID<N>, Transmission<N>>>>,
 }
@@ -42,7 +57,34 @@ impl<N: Network> Default for Storage<N> {
 impl<N: Network> Storage<N> {
     /// Initializes a new instance of storage.
     pub fn new() -> Self {
-        Self { rounds: Default::default(), certificates: Default::default(), transmissions: Default::default() }
+        Self {
+            rounds: Default::default(),
+            certificates: Default::default(),
+            batch_ids: Default::default(),
+            transmissions: Default::default(),
+        }
+    }
+}
+
+impl<N: Network> Storage<N> {
+    /// Returns an iterator over the `rounds` map.
+    pub fn rounds_iter(&self) -> impl Iterator<Item = (u64, IndexSet<(Field<N>, Field<N>, Address<N>)>)> {
+        self.rounds.read().clone().into_iter()
+    }
+
+    /// Returns an iterator over the `certificates` map.
+    pub fn certificates_iter(&self) -> impl Iterator<Item = (Field<N>, BatchCertificate<N>)> {
+        self.certificates.read().clone().into_iter()
+    }
+
+    /// Returns an iterator over the `batch IDs` map.
+    pub fn batch_ids_iter(&self) -> impl Iterator<Item = (Field<N>, u64)> {
+        self.batch_ids.read().clone().into_iter()
+    }
+
+    /// Returns an iterator over the `transmissions` map.
+    pub fn transmissions_iter(&self) -> impl Iterator<Item = (TransmissionID<N>, Transmission<N>)> {
+        self.transmissions.read().clone().into_iter()
     }
 }
 
@@ -74,18 +116,21 @@ impl<N: Network> Storage<N> {
         // Retrieve the certificates.
         let certificates = entries
             .iter()
-            .flat_map(|(certificate_id, _)| self.certificates.read().get(certificate_id).cloned())
+            .flat_map(|(certificate_id, _, _)| self.certificates.read().get(certificate_id).cloned())
             .collect();
         // Return the certificates.
         Some(certificates)
     }
 
-    /// Inserts the given `round` to (`certificate ID`, `certificate`) entry into storage.
+    /// Inserts the given `certificate` into storage.
+    /// This method triggers updates to the `rounds`, `certificates`, and `batch_ids` maps.
     pub fn insert_certificate(&self, certificate: BatchCertificate<N>) -> Result<()> {
         // Retrieve the round.
         let round = certificate.round();
         // Compute the certificate ID.
         let certificate_id = certificate.to_id()?;
+        // Retrieve the batch ID.
+        let batch_id = certificate.batch_id();
         // Compute the address of the batch creator.
         let address = certificate.to_address();
         // Ensure the certificate ID does not already exist in storage.
@@ -101,9 +146,38 @@ impl<N: Network> Storage<N> {
         // TODO (howardwu): Ensure the previous certificates have reached 2f+1. If not, do not store.
 
         // Insert the round to certificate ID entry.
-        self.rounds.write().entry(round).or_default().insert((certificate_id, address));
+        self.rounds.write().entry(round).or_default().insert((certificate_id, batch_id, address));
         // Insert the certificate.
         self.certificates.write().insert(certificate_id, certificate);
+        // Insert the batch ID.
+        self.batch_ids.write().insert(batch_id, round);
+        Ok(())
+    }
+
+    /// Removes the given `certificate ID` from storage.
+    /// This method triggers updates to the `rounds`, `certificates`, and `batch_ids` maps.
+    pub fn remove_certificate(&self, certificate_id: Field<N>) -> Result<()> {
+        // Retrieve the certificate.
+        let Some(certificate) = self.get_certificate(certificate_id) else {
+            bail!("Certificate {certificate_id} does not exist in storage");
+        };
+        // Retrieve the round.
+        let round = certificate.round();
+        // Retrieve the batch ID.
+        let batch_id = certificate.batch_id();
+        // Compute the address of the batch creator.
+        let address = certificate.to_address();
+
+        // Remove the round to certificate ID entry.
+        self.rounds.write().entry(round).or_default().remove(&(certificate_id, batch_id, address));
+        // If the round is empty, remove it.
+        if self.rounds.read().get(&round).map_or(false, |entries| entries.is_empty()) {
+            self.rounds.write().remove(&round);
+        }
+        // Remove the certificate.
+        self.certificates.write().remove(&certificate_id);
+        // Remove the batch ID.
+        self.batch_ids.write().remove(&batch_id);
         Ok(())
     }
 }
@@ -131,5 +205,95 @@ impl<N: Network> Storage<N> {
     ) -> Option<Transmission<N>> {
         // Insert the transmission.
         self.transmissions.write().insert(transmission_id.into(), transmission)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use snarkvm::prelude::TestRng;
+
+    use indexmap::indexset;
+
+    type CurrentNetwork = snarkvm::prelude::Testnet3;
+
+    /// Returns `true` if the storage is empty.
+    fn is_empty<N: Network>(storage: &Storage<N>) -> bool {
+        storage.rounds.read().is_empty()
+            && storage.certificates.read().is_empty()
+            && storage.batch_ids.read().is_empty()
+            && storage.transmissions.read().is_empty()
+    }
+
+    /// Asserts that the storage matches the expected layout.
+    fn assert_storage<N: Network>(
+        storage: &Storage<N>,
+        rounds: Vec<(u64, IndexSet<(Field<N>, Field<N>, Address<N>)>)>,
+        certificates: Vec<(Field<N>, BatchCertificate<N>)>,
+        batch_ids: Vec<(Field<N>, u64)>,
+        transmissions: Vec<(TransmissionID<N>, Transmission<N>)>,
+    ) {
+        // Ensure the rounds are well-formed.
+        assert_eq!(storage.rounds_iter().collect::<Vec<_>>(), rounds);
+        // Ensure the certificates are well-formed.
+        assert_eq!(storage.certificates_iter().collect::<Vec<_>>(), certificates);
+        // Ensure the batch IDs are well-formed.
+        assert_eq!(storage.batch_ids_iter().collect::<Vec<_>>(), batch_ids);
+        // Ensure the transmissions are well-formed.
+        assert_eq!(storage.transmissions_iter().collect::<Vec<_>>(), transmissions);
+    }
+
+    #[test]
+    fn test_certificate_insert_remove() {
+        let rng = &mut TestRng::default();
+
+        // Create a new storage.
+        let storage = Storage::<CurrentNetwork>::new();
+        // Ensure the storage is empty.
+        assert!(is_empty(&storage));
+
+        // Create a new certificate.
+        let certificate = snarkvm::ledger::narwhal::batch_certificate::test_helpers::sample_batch_certificate(rng);
+        // Compute the certificate ID.
+        let certificate_id = certificate.to_id().unwrap();
+        // Retrieve the round.
+        let round = certificate.round();
+        // Retrieve the batch ID.
+        let batch_id = certificate.batch_id();
+        // Compute the address of the batch creator.
+        let address = certificate.to_address();
+
+        // Insert the certificate.
+        storage.insert_certificate(certificate.clone()).unwrap();
+        // Ensure the storage is not empty.
+        assert!(!is_empty(&storage));
+        // Ensure the certificate is stored in the correct round.
+        assert_eq!(storage.get_round(round), Some(indexset! { certificate.clone() }));
+
+        // Check that the underlying storage representation is correct.
+        {
+            // Construct the expected layout for 'rounds'.
+            let rounds = vec![(round, indexset! { (certificate_id, batch_id, address) })];
+            // Construct the expected layout for 'certificates'.
+            let certificates = vec![(certificate_id, certificate.clone())];
+            // Construct the expected layout for 'batch_ids'.
+            let batch_ids = vec![(batch_id, round)];
+            // Construct the expected layout for 'transmissions'.
+            let transmissions = vec![];
+            // Assert the storage is well-formed.
+            assert_storage(&storage, rounds, certificates, batch_ids, transmissions);
+        }
+
+        // Retrieve the certificate.
+        let candidate_certificate = storage.get_certificate(certificate_id).unwrap();
+        // Ensure the retrieved certificate is the same as the inserted certificate.
+        assert_eq!(certificate, candidate_certificate);
+
+        // Remove the certificate.
+        storage.remove_certificate(certificate_id).unwrap();
+        // Ensure the storage is empty.
+        assert!(is_empty(&storage));
+        // Ensure the certificate is no longer stored in the round.
+        assert_eq!(storage.get_round(round), None);
     }
 }
