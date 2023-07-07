0f9a2bfa8812781035ff07f761b42a65d74ea48b
Jos Dehaes <jos.dehaes@gmail.com>
2023-07-07 14:59:48 +0200

    feat: simple test for partition

--- node/narwhal/src/helpers/partition.rs
+++ node/narwhal/src/helpers/partition.rs
@@ -69,3 +69,27 @@ pub fn assign_to_workers<N: Network>(
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use snarkvm::prelude::coinbase::PuzzleCommitment;
+
+    type CurrentNetwork = snarkvm::prelude::Testnet3;
+
+    #[test]
+    fn test_worker_assignment() {
+        let data = "Hello Aleo".as_bytes();
+        let sha = double_sha256(data);
+        assert_eq!(sha, [
+            113, 157, 210, 34, 60, 51, 220, 8, 63, 213, 79, 8, 117, 190, 134, 206, 127, 197, 21, 180, 116, 49, 218,
+            150, 49, 116, 116, 38, 244, 135, 215, 14
+        ]);
+        let hash = sha256d_to_u128(data);
+        assert_eq!(hash, 274520597840828436951879875061540363633u128);
+        let transmission_id: TransmissionID<CurrentNetwork> = TransmissionID::Solution(PuzzleCommitment::default());
+        let worker_id = assign_to_worker(transmission_id, 5).unwrap();
+        assert_eq!(worker_id, 4);
+    }
+}
+
