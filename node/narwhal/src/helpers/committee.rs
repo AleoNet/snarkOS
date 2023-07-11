d5bce5851982dbff676920aeca98a421861b66b1
Howard Wu <9260812+howardwu@users.noreply.github.com>
2023-07-11 14:23:25 -0700

    Add distribution tests

--- node/narwhal/src/helpers/committee.rs
+++ node/narwhal/src/helpers/committee.rs
@@ -57,12 +57,12 @@ impl<N: Network> Committee<N> {
 
 impl<N: Network> Committee<N> {
     /// Returns the current round number.
-    pub fn round(&self) -> u64 {
+    pub const fn round(&self) -> u64 {
         self.round
     }
 
     /// Returns the committee members alongside their stake.
-    pub fn members(&self) -> &IndexMap<Address<N>, u64> {
+    pub const fn members(&self) -> &IndexMap<Address<N>, u64> {
         &self.members
     }
 
@@ -110,7 +110,7 @@ impl<N: Network> Committee<N> {
     }
 
     /// Returns the total amount of stake in the committee `(3f + 1)`.
-    pub fn total_stake(&self) -> u64 {
+    pub const fn total_stake(&self) -> u64 {
         self.total_stake
     }
 }
@@ -119,17 +119,12 @@ impl<N: Network> Committee<N> {
     /// Returns the leader address for the current round.
     /// Note: This method returns a deterministic result that is SNARK-friendly.
     pub fn get_leader(&self) -> Result<Address<N>> {
-        // Set the current round.
-        let current_round = self.round;
-        // Set the previous round.
-        let previous_round = current_round.saturating_sub(1);
-
         // Retrieve the total stake of the committee.
         let total_stake = self.total_stake();
         // Construct the round seed.
-        let seed = [previous_round, current_round, total_stake].map(Field::from_u64);
+        let seed = [self.round, total_stake].map(Field::from_u64);
         // Hash the round seed.
-        let hash = Literal::Group(N::hash_to_group_psd4(&seed)?);
+        let hash = Literal::Field(N::hash_to_group_psd2(&seed)?.to_x_coordinate());
         // Compute the stake index from the hash output.
         let stake_index = match hash.downcast_lossy(LiteralType::U64)? {
             Literal::U64(output) => (*output) % total_stake,
@@ -140,7 +135,7 @@ impl<N: Network> Committee<N> {
         let mut leader = None;
         // Initialize a tracker for the current stake index.
         let mut current_stake_index = 0u64;
-        // Sort the committee members in decreasing order of their stake, tie-breaking with addresses.
+        // Sort the committee members.
         let candidates = self.sorted_members();
         // Determine the leader of the previous round.
         for (candidate, stake) in candidates {
@@ -160,15 +155,14 @@ impl<N: Network> Committee<N> {
     /// Returns the committee members sorted by stake in decreasing order.
     /// For members with matching stakes, we further sort by their address' x-coordinate in decreasing order.
     /// Note: This ensures the method returns a deterministic result that is SNARK-friendly.
-    fn sorted_members(&self) -> Vec<(Address<N>, u64)> {
-        let mut members = self.members.iter().map(|(address, stake)| (*address, *stake)).collect::<Vec<_>>();
-        members.sort_by(|(address1, stake1), (address2, stake2)| {
+    fn sorted_members(&self) -> indexmap::map::IntoIter<Address<N>, u64> {
+        let members = self.members.clone();
+        members.sorted_by(|address1, stake1, address2, stake2| {
             // Sort by stake in decreasing order.
             let cmp = stake2.cmp(stake1);
             // If the stakes are equal, sort by x-coordinate in decreasing order.
             if cmp == Ordering::Equal { address2.to_x_coordinate().cmp(&address1.to_x_coordinate()) } else { cmp }
-        });
-        members
+        })
     }
 }
 
@@ -193,6 +187,7 @@ pub mod test_helpers {
     use snarkvm::prelude::{Address, TestRng};
 
     use indexmap::IndexMap;
+    use rand_distr::{Distribution, Exp};
 
     type CurrentNetwork = snarkvm::prelude::Testnet3;
 
@@ -200,12 +195,116 @@ pub mod test_helpers {
     pub fn sample_committee(rng: &mut TestRng) -> Committee<CurrentNetwork> {
         // Sample the members.
         let mut members = IndexMap::new();
-        members.insert(Address::<CurrentNetwork>::new(rng.gen()), 1000);
-        members.insert(Address::<CurrentNetwork>::new(rng.gen()), 1000);
-        members.insert(Address::<CurrentNetwork>::new(rng.gen()), 1000);
-        members.insert(Address::<CurrentNetwork>::new(rng.gen()), 1000);
+        for _ in 0..4 {
+            members.insert(Address::<CurrentNetwork>::new(rng.gen()), MIN_STAKE);
+        }
         // Return the committee.
         Committee::<CurrentNetwork>::new(1, members).unwrap()
+    }
+
+    /// Samples a random committee.
+    pub fn sample_committee_custom(num_members: u16, rng: &mut TestRng) -> Committee<CurrentNetwork> {
+        assert!(num_members >= 4);
+        // Set the minimum amount staked in the node.
+        const MIN_STAKE: u64 = 1_000_000_000_000;
+        // Set the maximum amount staked in the node.
+        const MAX_STAKE: u64 = 100_000_000_000_000;
+        // Initialize the Exponential distribution.
+        let distribution = Exp::new(2.0).unwrap();
+        // Initialize an RNG for the stake.
+        let range = (MAX_STAKE - MIN_STAKE) as f64;
+        // Sample the members.
+        let mut members = IndexMap::new();
+        // Add in the minimum and maximum staked nodes.
+        members.insert(Address::<CurrentNetwork>::new(rng.gen()), MIN_STAKE);
+        while members.len() < num_members as usize - 1 {
+            loop {
+                let stake = MIN_STAKE as f64 + range * distribution.sample(rng);
+                if stake >= MIN_STAKE as f64 && stake <= MAX_STAKE as f64 {
+                    members.insert(Address::<CurrentNetwork>::new(rng.gen()), stake as u64);
+                    break;
+                }
+            }
+        }
+        members.insert(Address::<CurrentNetwork>::new(rng.gen()), MAX_STAKE);
+        // Return the committee.
+        Committee::<CurrentNetwork>::new(1, members).unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use snarkvm::prelude::TestRng;
+
+    use parking_lot::RwLock;
+    use rayon::prelude::*;
+    use std::sync::Arc;
+
+    type CurrentNetwork = snarkvm::prelude::Testnet3;
+
+    /// Checks the leader distribution.
+    fn check_leader_distribution(committee: Committee<CurrentNetwork>, num_rounds: u64, tolerance_percent: f64) {
+        // Initialize a tracker for the leaders.
+        let leaders = Arc::new(RwLock::new(IndexMap::<Address<CurrentNetwork>, i64>::new()));
+        // Iterate through the rounds.
+        (1..=num_rounds).into_par_iter().for_each(|round| {
+            // Construct the committee for the round.
+            let committee = Committee::<CurrentNetwork>::new(round, committee.members.clone()).unwrap();
+            // Compute the leader.
+            let leader = committee.get_leader().unwrap();
+            // Increment the leader count for the current leader.
+            leaders.write().entry(leader).or_default().add_assign(1);
+        });
+        let leaders = leaders.read();
+        // Ensure the leader distribution is uniform.
+        for (i, (address, stake)) in committee.members.iter().enumerate() {
+            // Get the leader count for the validator.
+            let Some(leader_count) = leaders.get(address) else {
+                println!("{i}: 0 rounds");
+                continue;
+            };
+            // Compute the target leader percentage.
+            let target_percent = *stake as f64 / committee.total_stake() as f64 * 100f64;
+            // Compute the actual leader percentage for the validator.
+            let leader_percent = (*leader_count as f64 / num_rounds as f64) * 100f64;
+            // Compute the error percentage from the target.
+            let error_percent = (leader_percent - target_percent) / target_percent * 100f64;
+
+            // Print the results.
+            let stake = stake / 1_000_000; // credits
+            println!("{i}: {stake}, {leader_count}, {target_percent:.3}%, {leader_percent:.3}%, {error_percent:.2}%");
+            if target_percent > 0.5 {
+                assert!(error_percent.abs() < tolerance_percent);
+            }
+        }
+    }
+
+    #[test]
+    fn test_get_leader_distribution_simple() {
+        // Initialize the RNG.
+        let rng = &mut TestRng::default();
+        // Set the number of rounds.
+        const NUM_ROUNDS: u64 = 256 * 100;
+        // Sample a committee.
+        let committee = crate::helpers::committee::test_helpers::sample_committee(rng);
+        // Check the leader distribution.
+        check_leader_distribution(committee, NUM_ROUNDS, 2.0);
+    }
+
+    #[test]
+    fn test_get_leader_distribution() {
+        // Initialize the RNG.
+        let rng = &mut TestRng::default();
+        // Set the number of rounds.
+        const NUM_ROUNDS: u64 = 256 * 1_000;
+        // Sample the number of members.
+        // let num_members = rng.gen_range(4..50);
+        let num_members = 50;
+        // Sample a committee.
+        let committee = crate::helpers::committee::test_helpers::sample_committee_custom(num_members, rng);
+        // Check the leader distribution.
+        check_leader_distribution(committee, NUM_ROUNDS, 5.0);
     }
 }
 
