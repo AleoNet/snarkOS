b7b499a8902c7a500d7dfe7d656186f31914073f
Jos Dehaes <jos.dehaes@gmail.com>
2023-07-18 10:58:16 +0200

    tests: add quorum tests

--- node/narwhal/tests/bft_e2e.rs
+++ node/narwhal/tests/bft_e2e.rs
@@ -15,6 +15,10 @@
 mod common;
 
 use crate::common::primary::{TestNetwork, TestNetworkConfig};
+use deadline::deadline;
+use snarkos_node_narwhal::MAX_BATCH_DELAY;
+use std::time::Duration;
+use tokio::time::sleep;
 
 #[tokio::test(flavor = "multi_thread")]
 #[ignore = "long-running e2e test"]
@@ -33,4 +37,84 @@ async fn test_state_coherence() {
     network.start().await;
 
     std::future::pending::<()>().await;
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_quorum_threshold() {
+    // Start N nodes but don't connect them.
+    const N: u16 = 4;
+    let mut network = TestNetwork::new(TestNetworkConfig {
+        num_nodes: N,
+        bft: true,
+        connect_all: false,
+        fire_cannons: false,
+        // Set this to Some(0..=4) to see the logs.
+        log_level: None,
+        log_connections: true,
+    });
+    network.start().await;
+
+    // Check each node is at round 1 (0 is genesis).
+    for validators in network.validators.values() {
+        assert_eq!(validators.primary.current_round(), 1);
+    }
+
+    // Start the cannons for node 0.
+    network.fire_cannons_at(0);
+
+    sleep(Duration::from_millis(MAX_BATCH_DELAY * 2)).await;
+
+    // Check each node is still at round 1.
+    for validator in network.validators.values() {
+        assert_eq!(validator.primary.current_round(), 1);
+    }
+
+    // Connect the first two nodes and start the cannons for node 1.
+    network.connect_validators(0, 1).await;
+    network.fire_cannons_at(1);
+
+    sleep(Duration::from_millis(MAX_BATCH_DELAY * 2)).await;
+
+    // Check each node is still at round 1.
+    for validator in network.validators.values() {
+        assert_eq!(validator.primary.current_round(), 1);
+    }
+
+    // Connect the third node and start the cannons for it.
+    network.connect_validators(0, 2).await;
+    network.connect_validators(1, 2).await;
+    network.fire_cannons_at(2);
+
+    // Check the nodes reach quorum and advance through the rounds.
+    const TARGET_ROUND: u64 = 4;
+    deadline!(Duration::from_secs(20), move || { network.is_round_reached(TARGET_ROUND) });
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_quorum_break() {
+    // Start N nodes, connect them and start the cannons for each.
+    const N: u16 = 4;
+    let mut network = TestNetwork::new(TestNetworkConfig {
+        num_nodes: N,
+        bft: true,
+        connect_all: true,
+        fire_cannons: true,
+        // Set this to Some(0..=4) to see the logs.
+        log_level: None,
+        log_connections: true,
+    });
+    network.start().await;
+
+    // Check the nodes have started advancing through the rounds.
+    const TARGET_ROUND: u64 = 4;
+    // Note: cloning the network is fine because the primaries it wraps are `Arc`ed.
+    let network_clone = network.clone();
+    deadline!(Duration::from_secs(20), move || { network_clone.is_round_reached(TARGET_ROUND) });
+
+    // Break the quorum by disconnecting two nodes.
+    const NUM_NODES: u16 = 2;
+    network.disconnect(NUM_NODES).await;
+
+    // Check the nodes have stopped advancing through the rounds.
+    assert!(network.is_halted().await);
 }
