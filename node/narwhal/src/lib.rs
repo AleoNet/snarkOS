93258824dda61336fd2738334a5e7755cedd4057
Howard Wu <9260812+howardwu@users.noreply.github.com>
2023-08-06 17:09:02 -0700

    Increase range for expiry

--- node/narwhal/src/lib.rs
+++ node/narwhal/src/lib.rs
@@ -46,7 +46,7 @@ pub const MAX_EXPIRATION_TIME_IN_SECS: i64 = 10; // seconds
 /// The maximum number of rounds to store before garbage collecting.
 pub const MAX_GC_ROUNDS: u64 = 50; // rounds
 /// The maximum number of seconds allowed for the leader to send their certificate.
-pub const MAX_LEADER_CERTIFICATE_DELAY: i64 = 2 * MAX_BATCH_DELAY as i64 / 1000; // seconds
+pub const MAX_LEADER_CERTIFICATE_DELAY: i64 = 4 * MAX_BATCH_DELAY as i64 / 1000; // seconds
 /// The maximum number of seconds before the timestamp is considered expired.
 pub const MAX_TIMESTAMP_DELTA_IN_SECS: i64 = 10; // seconds
 /// The maximum number of transmissions allowed in a batch.
