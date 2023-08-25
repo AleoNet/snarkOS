3fcc84bc34a3d129754ee0a7941f10528edf08dd
Niklas <niklas@long.ch>
2023-08-25 09:54:58 +0200

    tests: noise codec disconnect roundtrip

--- node/narwhal/src/helpers/codec.rs
+++ node/narwhal/src/helpers/codec.rs
@@ -328,3 +328,76 @@ impl<N: Network> Decoder for NoiseCodec<N> {
         Ok(msg)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::{Disconnect, DisconnectReason};
+
+    use snow::{params::NoiseParams, Builder};
+
+    type CurrentNetwork = snarkvm::prelude::Testnet3;
+
+    fn handshake_xx() -> (NoiseCodec<CurrentNetwork>, NoiseCodec<CurrentNetwork>) {
+        let params: NoiseParams = "Noise_XX_25519_ChaChaPoly_BLAKE2s".parse().unwrap();
+
+        let initiator_builder = Builder::new(params.clone());
+        let initiator_kp = initiator_builder.generate_keypair().unwrap();
+        let initiator = initiator_builder.local_private_key(&initiator_kp.private).build_initiator().unwrap();
+
+        let responder_builder = Builder::new(params);
+        let responder_kp = responder_builder.generate_keypair().unwrap();
+        let responder = responder_builder.local_private_key(&responder_kp.private).build_responder().unwrap();
+
+        let mut initiator_codec = NoiseCodec::new(NoiseState::Handshake(Box::new(initiator)));
+        let mut responder_codec = NoiseCodec::new(NoiseState::Handshake(Box::new(responder)));
+
+        let mut ciphertext = BytesMut::new();
+
+        // -> e
+        assert!(initiator_codec.encode(EventOrBytes::Bytes(Bytes::new()), &mut ciphertext).is_ok());
+        assert!(
+            matches!(responder_codec.decode(&mut ciphertext).unwrap().unwrap(), EventOrBytes::Bytes(bytes) if bytes.is_empty())
+        );
+
+        // <- e, ee, s, es
+        assert!(responder_codec.encode(EventOrBytes::Bytes(Bytes::new()), &mut ciphertext).is_ok());
+        assert!(
+            matches!(initiator_codec.decode(&mut ciphertext).unwrap().unwrap(), EventOrBytes::Bytes(bytes) if bytes.is_empty())
+        );
+
+        // -> s, se
+        assert!(initiator_codec.encode(EventOrBytes::Bytes(Bytes::new()), &mut ciphertext).is_ok());
+        assert!(
+            matches!(responder_codec.decode(&mut ciphertext).unwrap().unwrap(), EventOrBytes::Bytes(bytes) if bytes.is_empty())
+        );
+
+        initiator_codec.noise_state = initiator_codec.noise_state.into_post_handshake_state();
+        responder_codec.noise_state = responder_codec.noise_state.into_post_handshake_state();
+
+        (initiator_codec, responder_codec)
+    }
+
+    fn assert_roundtrip(msg: EventOrBytes<CurrentNetwork>) {
+        let (mut initiator_codec, mut responder_codec) = handshake_xx();
+        let mut ciphertext = BytesMut::new();
+
+        assert!(initiator_codec.encode(msg.clone(), &mut ciphertext).is_ok());
+        assert_eq!(responder_codec.decode(&mut ciphertext).unwrap().unwrap(), msg);
+    }
+
+    macro_rules! test_disconnect_roundtrip {
+        ($($reason:ident), *) => {
+            #[test]
+            fn disconnect_roundtrip() {
+                $(
+                    let disconnect = EventOrBytes::Event(Event::Disconnect(Disconnect { reason: DisconnectReason::$reason }));
+                    assert_roundtrip(disconnect);
+                )*
+            }
+        };
+    }
+
+    test_disconnect_roundtrip!(InvalidChallengeResponse, NoReasonGiven, ProtocolViolation, OutdatedClientVersion);
+}
