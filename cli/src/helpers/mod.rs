0a6af4ccd31ac929ee5e1899e519d826c1528b8b
Howard Wu <9260812+howardwu@users.noreply.github.com>
2023-09-26 18:29:07 -0700

    Update cli/src/helpers/mod.rs
    Signed-off-by: Howard Wu <9260812+howardwu@users.noreply.github.com>

--- cli/src/helpers/mod.rs
+++ cli/src/helpers/mod.rs
@@ -153,7 +153,7 @@ pub(crate) fn check_validator_machine(node_type: NodeType, is_dev: bool) {
             let message = "⚠️  This machine does not have an NVMe drive, and is insufficient for a validator (NVMe SSD required)\n".to_string();
             match is_dev {
                 true => println!("{}", message.yellow().bold()),
-                false => panic!("{message} in production mode"),
+                false => println!("{message} in production mode"),
             }
         }
     }
