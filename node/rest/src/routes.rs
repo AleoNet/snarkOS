37efa0507fca34be2e713e72d270036fa6fb70e1
Alvin Reyes <me@areyes.onl>
2023-10-28 23:46:16 -0400

    fix

--- node/rest/src/routes.rs
+++ node/rest/src/routes.rs
@@ -132,9 +132,11 @@ impl<N: Network, C: ConsensusStorage<N>, R: Routing<N>> Rest<N, C, R> {
         }
 
         let blocks = cfg_into_iter!((start_height..end_height))
-            .map(|height| {rest.ledger.get_committee(height),height})
-            .collect::<Result<Vec<_,_>, _>>()?;
-            
+            .map(|height| rest.ledger.get_committee(height))
+            .collect::<Result<Vec<_>, _>>()?;
+
+        
+
         Ok(ErasedJson::pretty(blocks))
     }
 
