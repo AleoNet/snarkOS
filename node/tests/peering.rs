a0b6ef6beda476680e6a0e4a03024e8edf42d7e5
Niklas <niklas@long.ch>
2023-12-07 20:51:57 +0100

    test: all node types in unsolicited `PeerResponse`

--- node/tests/peering.rs
+++ node/tests/peering.rs
@@ -14,7 +14,7 @@
 
 #[allow(dead_code)]
 mod common;
-use common::{node::*, test_peer::TestPeer};
+use common::test_peer::TestPeer;
 
 use snarkos_node_router::{
     messages::{Message, PeerResponse},
@@ -23,48 +23,60 @@ use snarkos_node_router::{
 use snarkos_node_tcp::P2P;
 
 use deadline::deadline;
+use paste::paste;
 use pea2pea::{protocols::Writing, Pea2Pea};
 use std::time::Duration;
 
-#[tokio::test]
-async fn reject_unsolicited_peer_response() {
-    // Spin up a full node.
-    let node = validator().await;
+macro_rules! test_reject_unsolicited_peer_response {
+    ($($node_type:ident),*) => {
+        $(
+            paste! {
+                #[tokio::test]
+                async fn [<$node_type _rejects_unsolicited_peer_response>]() {
+                    // Spin up a full node.
+                    let node = $crate::common::node::$node_type().await;
 
-    // Spin up a test peer (synthetic node).
-    let peer = TestPeer::validator().await;
-    let peer_addr = peer.node().listening_addr().unwrap();
+                    // Spin up a test peer (synthetic node), it doesn't really matter what type it is.
+                    let peer = TestPeer::validator().await;
+                    let peer_addr = peer.node().listening_addr().unwrap();
 
-    // Connect the node to the test peer.
-    node.router().connect(peer_addr).unwrap().await.unwrap();
+                    // Connect the node to the test peer.
+                    node.router().connect(peer_addr).unwrap().await.unwrap();
 
-    // Check the peer counts.
-    let node_clone = node.clone();
-    deadline!(Duration::from_secs(5), move || node_clone.router().number_of_connected_peers() == 1);
-    let node_clone = node.clone();
-    deadline!(Duration::from_secs(5), move || node_clone.tcp().num_connected() == 1);
-    let peer_clone = peer.clone();
-    deadline!(Duration::from_secs(5), move || peer_clone.node().num_connected() == 1);
+                    // Check the peer counts.
+                    let node_clone = node.clone();
+                    deadline!(Duration::from_secs(5), move || node_clone.router().number_of_connected_peers() == 1);
+                    let node_clone = node.clone();
+                    deadline!(Duration::from_secs(5), move || node_clone.tcp().num_connected() == 1);
+                    let peer_clone = peer.clone();
+                    deadline!(Duration::from_secs(5), move || peer_clone.node().num_connected() == 1);
 
-    // Check the candidate peers.
-    assert_eq!(node.router().number_of_candidate_peers(), 0);
+                    // Check the candidate peers.
+                    assert_eq!(node.router().number_of_candidate_peers(), 0);
 
-    let peers = vec!["1.1.1.1:1111".parse().unwrap(), "2.2.2.2:2222".parse().unwrap()];
+                    let peers = vec!["1.1.1.1:1111".parse().unwrap(), "2.2.2.2:2222".parse().unwrap()];
 
-    // Send a `PeerResponse` to the node.
-    assert!(
-        peer.unicast(
-            *peer.node().connected_addrs().first().unwrap(),
-            Message::PeerResponse(PeerResponse { peers: peers.clone() })
-        )
-        .is_ok()
-    );
+                    // Send a `PeerResponse` to the node.
+                    assert!(
+                        peer.unicast(
+                            *peer.node().connected_addrs().first().unwrap(),
+                            Message::PeerResponse(PeerResponse { peers: peers.clone() })
+                        )
+                        .is_ok()
+                    );
 
-    let node_clone = node.clone();
-    deadline!(Duration::from_secs(5), move || node_clone.router().number_of_connected_peers() == 0);
+                    // Wait for the peer to be disconnected for a protocol violation.
+                    let node_clone = node.clone();
+                    deadline!(Duration::from_secs(5), move || node_clone.router().number_of_connected_peers() == 0);
 
-    // Make sure the sent addresses weren't inserted in the candidate peers.
-    for peer in peers {
-        assert!(!node.router().candidate_peers().contains(&peer));
-    }
+                    // Make sure the sent addresses weren't inserted in the candidate peers.
+                    for peer in peers {
+                        assert!(!node.router().candidate_peers().contains(&peer));
+                    }
+                }
+            }
+        )*
+    };
 }
+
+test_reject_unsolicited_peer_response!(client, prover, validator);
