424ad4a7a73c3573d983798d1a2bff71147c74a0
raychu86 <14917648+raychu86@users.noreply.github.com>
2024-02-08 18:07:21 -0800

    Minor tweaks

--- node/bft/src/helpers/storage.rs
+++ node/bft/src/helpers/storage.rs
@@ -164,14 +164,20 @@ impl<N: Network> Storage<N> {
 
         // Retrieve the current committee.
         let current_committee = self.ledger.current_committee()?;
-        // If the primary is behind the current committee's starting round, sync with the latest block. 
-        if next_round < current_committee.starting_round() {
-            info!("Primary is behind the current committee's starting round ({}). Syncing with the latest block...", 
-            current_committee.starting_round()
-            ); 
-            let latest_block = self.ledger.latest_block();
-            self.sync_round_with_block(latest_block.round());
-            return Ok(latest_block.round()); 
+        // Retrieve the current committee's starting round.
+        let starting_round = current_committee.starting_round();
+        // If the primary is behind the current committee's starting round, sync with the latest block.
+        if next_round < starting_round {
+            // Retrieve the latest block round.
+            let latest_block_round = self.ledger.latest_round();
+            // Log the round sync.
+            info!(
+                "Syncing primary round ({next_round}) with the current committee's starting round ({starting_round}). Syncing with the latest block round {latest_block_round}..."
+            );
+            // Sync the round with the latest block.
+            self.sync_round_with_block(latest_block_round);
+            // Return the latest block round.
+            return Ok(latest_block_round);
         }
 
         // Update the storage to the next round.
