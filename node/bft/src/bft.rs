31ae363045fedcb07bf01440e8424434970ed8ac
raychu86 <14917648+raychu86@users.noreply.github.com>
2024-03-05 16:25:43 -0800

    Resolve cherry-pick for f033e7f

--- node/bft/src/bft.rs
+++ node/bft/src/bft.rs
@@ -803,62 +803,24 @@ impl<N: Network> BFT<N> {
         });
     }
 
-    /// Syncs the BFT DAG with the given leader certificates and batch certificates.
+    /// Syncs the BFT DAG with the given batch certificates. These batch certificates **must**
+    /// already exist in the ledger.
     ///
-    /// This method starts by inserting all certificates (except the latest leader certificate)
-    /// into the DAG. Then, it commits all leader certificates (except the latest leader certificate).
-    /// Finally, it updates the DAG with the latest leader certificate.
+    /// This method commits all the certificates into the dag.
+    /// Note that there is no need to insert the certificates into the DAG, because these certificates
+    /// already exist in the ledger and therefor do not need to be re-ordered into future
+    /// committed subdags.
     async fn sync_bft_dag_at_bootup(
         &self,
-        leader_certificates: Vec<(BatchCertificate<N>, IndexSet<Field<N>>)>,
+        _leader_certificates: Vec<(BatchCertificate<N>, IndexSet<Field<N>>)>,
         certificates: Vec<BatchCertificate<N>>,
     ) {
-        // Split the leader certificates into past leader certificates, the latest leader certificate, and the election certificate IDs.
-        let (past_leader_certificates, leader_certificate, election_certificate_ids) = {
-            // Compute the penultimate index.
-            let index = leader_certificates.len().saturating_sub(1);
-            // Split the leader certificates.
-            let (past, latest) = leader_certificates.split_at(index);
-            debug_assert!(latest.len() == 1, "There should only be one latest leader certificate");
-            // Retrieve the latest leader certificate.
-            match latest.first() {
-                Some((leader_certificate, election_certificate_ids)) => {
-                    (past, leader_certificate.clone(), election_certificate_ids.clone())
-                }
-                // If there is no latest leader certificate, return early.
-                None => return,
-            }
-        };
-        {
-            // Acquire the BFT write lock.
-            let mut dag = self.dag.write();
-            // Iterate over the certificates.
-            for certificate in certificates {
-                // If the certificate is not the latest leader certificate, insert it.
-                if leader_certificate.id() != certificate.id() {
-                    // Insert the certificate into the DAG.
-                    dag.insert(certificate);
-                }
-            }
+        // Acquire the BFT write lock.
+        let mut dag = self.dag.write();
 
-            // Acquire the last election certificate IDs.
-            let mut last_election_certificate_ids = self.last_election_certificate_ids.write();
-            // Iterate over the leader certificates.
-            for (leader_certificate, election_certificate_ids) in past_leader_certificates {
-                // Commit the leader certificate.
-                dag.commit(leader_certificate, self.storage().max_gc_rounds());
-                // Update the last election certificate IDs.
-                //
-                // Note: Because we will be committing the latest leader certificate after this,
-                // technically we do not need to be updating the last election certificate IDs
-                // for intermediate leader certificates. However, this is a safety mechanic to ensure completeness.
-                *last_election_certificate_ids = election_certificate_ids.clone();
-            }
-        }
-        // Commit the latest leader certificate.
-        if let Err(e) = self.commit_leader_certificate::<true, true>(leader_certificate, election_certificate_ids).await
-        {
-            error!("BFT failed to update the DAG with the latest leader certificate - {e}");
+        // Commit all the certificates excluding the latest leader certificate.
+        for certificate in certificates {
+            dag.commit(&certificate, self.storage().max_gc_rounds());
         }
     }
 
