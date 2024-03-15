1bca44de88376199bf90d746a9ce2f8416c2e0ea
raychu86 <14917648+raychu86@users.noreply.github.com>
2024-03-15 15:49:15 -0700

    Add correct flag for local devnet client node

--- devnet.sh
+++ devnet.sh
@@ -87,7 +87,7 @@ for client_index in "${client_indices[@]}"; do
   tmux new-window -t "devnet:$window_index" -n "window-$window_index"
 
   # Send the command to start the validator to the new window and capture output to the log file
-  tmux send-keys -t "devnet:window-$window_index" "snarkos start --nodisplay --dev $window_index --client --logfile $log_file" C-m
+  tmux send-keys -t "devnet:window-$window_index" "snarkos start --nodisplay --dev $window_index --dev-num-validators $total_validators --client --logfile $log_file" C-m
 done
 
 # Attach to the tmux session to view and interact with the windows
