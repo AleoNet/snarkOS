052b2874e668d15c1a285ef52074458f9396e8a1
raychu86 <14917648+raychu86@users.noreply.github.com>
2024-05-01 21:53:12 -0400

    Update filename in dev instances

--- node/bft/src/helpers/proposal_cache.rs
+++ node/bft/src/helpers/proposal_cache.rs
@@ -24,15 +24,17 @@ use std::{fs, path::PathBuf};
 
 // Returns the path where a proposal cache file may be stored.
 pub fn proposal_cache_path(network: u16, dev: Option<u16>) -> PathBuf {
+    const PROPOSAL_CACHE_FILE_NAME: &str = "current-proposal-cache";
+
     // Obtain the path to the ledger.
     let mut path = aleo_ledger_dir(network, StorageMode::from(dev));
     // Go to the folder right above the ledger.
     path.pop();
     // Append the proposal store's file name.
-    path.push(&format!(
-        "current-proposal-cache-{network}{}",
-        if let Some(id) = dev { format!("-{id}") } else { "".into() }
-    ));
+    match dev {
+        Some(id) => path.push(&format!(".{PROPOSAL_CACHE_FILE_NAME}-{}-{}", network, id)),
+        None => path.push(&format!("{PROPOSAL_CACHE_FILE_NAME}-{}", network)),
+    }
 
     path
 }
